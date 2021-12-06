6acd474dd8eb807525e03424559f859cb1d2af9a
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-06 13:56:00 +0200

    refactor

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use itertools::Itertools;
+use itertools::{iterate, Itertools};
 
 const DATA: &str = include_str!("data.txt");
 
@@ -13,35 +13,32 @@ fn parse(data: &'static str) -> HashMap<i8, usize> {
     data.trim().split(',').map(|v| v.parse().unwrap()).counts()
 }
 
-fn part_a(data: &'static str) -> usize {
-    let mut age_counts = parse(data);
-    for _ in 0..80 {
-        age_counts = age_counts
-            .iter()
-            .map(|(&age, &count)| (age - 1, count))
-            .collect();
-        if let Some(births) = age_counts.remove(&-1) {
-            *age_counts.entry(6).or_insert(0) += births;
-            age_counts.insert(8, births);
-        }
+fn next_state(age_counts: &HashMap<i8, usize>) -> HashMap<i8, usize> {
+    let mut age_counts: HashMap<_, _> = age_counts
+        .iter()
+        .map(|(&age, &count)| (age - 1, count))
+        .collect();
+    if let Some(births) = age_counts.remove(&-1) {
+        *age_counts.entry(6).or_insert(0) += births;
+        age_counts.insert(8, births);
     }
-    age_counts.values().sum()
+    age_counts
 }
 
-#[allow(dead_code)]
+fn part_a(data: &'static str) -> usize {
+    iterate(parse(data), next_state)
+        .nth(80)
+        .unwrap()
+        .values()
+        .sum()
+}
+
 fn part_b(data: &'static str) -> usize {
-    let mut age_counts = parse(data);
-    for _ in 0..256 {
-        age_counts = age_counts
-            .iter()
-            .map(|(&age, &count)| (age - 1, count))
-            .collect();
-        if let Some(births) = age_counts.remove(&-1) {
-            *age_counts.entry(6).or_insert(0) += births;
-            age_counts.insert(8, births);
-        }
-    }
-    age_counts.values().sum()
+    iterate(parse(data), next_state)
+        .nth(256)
+        .unwrap()
+        .values()
+        .sum()
 }
 
 #[cfg(test)]
