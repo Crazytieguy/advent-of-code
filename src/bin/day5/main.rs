73228410fa05a96a6e6dd5b49d4228e54350e443
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-06 04:09:03 +0200

    use vector math instead of madness

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,5 +1,4 @@
-use std::iter;
-
+use bevy_math::{ivec2, IVec2};
 use itertools::Itertools;
 
 const DATA: &str = include_str!("data.txt");
@@ -10,46 +9,21 @@ fn main() {
 }
 
 struct Vent {
-    x1: i32,
-    y1: i32,
-    x2: i32,
-    y2: i32,
+    start: IVec2,
+    stop: IVec2,
 }
 
-struct IncrDecrInclusiveRange {
-    state: i32,
-    stop: i32,
-    signum: i32,
-    done: bool,
-}
-
-impl IncrDecrInclusiveRange {
-    fn new(start: i32, stop: i32) -> Self {
-        Self {
-            state: start,
-            stop,
-            signum: (stop - start).signum(),
-            done: false,
+impl Vent {
+    fn points(&self) -> Vec<IVec2> {
+        let mut points = vec![];
+        let mut cur = self.start;
+        let direction = (self.stop - self.start).signum();
+        while cur != self.stop {
+            points.push(cur);
+            cur += direction;
         }
-    }
-}
-
-impl Iterator for IncrDecrInclusiveRange {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.state == self.stop {
-            if self.done {
-                None
-            } else {
-                self.done = true;
-                Some(self.state)
-            }
-        } else {
-            let res = self.state;
-            self.state += self.signum;
-            Some(res)
-        }
+        points.push(cur);
+        points
     }
 }
 
@@ -61,29 +35,26 @@ impl From<&'static str> for Vent {
             .map(|coord| coord.parse().unwrap())
             .collect_tuple()
             .unwrap();
-        Self { x1, y1, x2, y2 }
+        Self {
+            start: ivec2(x1, y1),
+            stop: ivec2(x2, y2),
+        }
     }
 }
 
 fn part_a(data: &'static str) -> usize {
     data.lines()
         .map(Vent::from)
-        .flat_map(|Vent { x1, x2, y1, y2 }| {
-            if x1 == x2 {
-                iter::repeat(x1)
-                    .zip(IncrDecrInclusiveRange::new(y1, y2))
-                    .collect()
-            } else if y1 == y2 {
-                IncrDecrInclusiveRange::new(x1, x2)
-                    .zip(iter::repeat(y1))
-                    .collect()
+        .flat_map(|vent| {
+            if vent.start.x == vent.stop.x || vent.start.y == vent.stop.y {
+                vent.points()
             } else {
                 vec![]
             }
         })
         .counts()
         .values()
-        .filter(|&v| *v >= 2)
+        .filter(|&&v| v >= 2)
         .count()
 }
 
@@ -91,24 +62,10 @@ fn part_a(data: &'static str) -> usize {
 fn part_b(data: &'static str) -> usize {
     data.lines()
         .map(Vent::from)
-        .flat_map(|Vent { x1, x2, y1, y2 }| {
-            if x1 == x2 {
-                iter::repeat(x1)
-                    .zip(IncrDecrInclusiveRange::new(y1, y2))
-                    .collect_vec()
-            } else if y1 == y2 {
-                IncrDecrInclusiveRange::new(x1, x2)
-                    .zip(iter::repeat(y1))
-                    .collect_vec()
-            } else {
-                IncrDecrInclusiveRange::new(x1, x2)
-                    .zip(IncrDecrInclusiveRange::new(y1, y2))
-                    .collect_vec()
-            }
-        })
+        .flat_map(|vent| vent.points())
         .counts()
         .values()
-        .filter(|&v| *v >= 2)
+        .filter(|&&v| v >= 2)
         .count()
 }
 
@@ -116,21 +73,6 @@ fn part_b(data: &'static str) -> usize {
 mod tests {
     use super::*;
     const SAMPLE_DATA: &str = include_str!("sample.txt");
-
-    #[test]
-    fn test_range() {
-        let mut range = IncrDecrInclusiveRange::new(0, 2);
-        assert_eq!(range.next(), Some(0));
-        assert_eq!(range.next(), Some(1));
-        assert_eq!(range.next(), Some(2));
-        assert_eq!(range.next(), None);
-
-        range = IncrDecrInclusiveRange::new(2, 0);
-        assert_eq!(range.next(), Some(2));
-        assert_eq!(range.next(), Some(1));
-        assert_eq!(range.next(), Some(0));
-        assert_eq!(range.next(), None);
-    }
 
     #[test]
     fn test_a() {
