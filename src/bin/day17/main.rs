9107db1d7a15166c9da867a27ead8e272e4a42f3
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-17 08:18:13 +0200

    refactoring

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -8,44 +8,32 @@ fn main() {
     println!("part b: {}", part_b(DATA));
 }
 
-fn parse(data: &'static str) -> (isize, isize, isize, isize) {
+fn parse(data: &'static str) -> [isize; 4] {
     let pat = Regex::new(r"target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)").unwrap();
-    let captures = pat.captures(data).unwrap();
-    (1..=4)
-        .map(|i| captures[i].parse().unwrap())
-        .collect_tuple()
-        .unwrap()
+    let caps = pat.captures(data).unwrap();
+    [&caps[1], &caps[2], &caps[3], &caps[4]].map(|s| s.parse().unwrap())
 }
 
 fn part_a(data: &'static str) -> usize {
-    let (_min_x, _max_x, min_y, _max_y) = parse(data);
+    let [_min_x, _max_x, min_y, _max_y] = parse(data);
     let vel_y = -1 - min_y;
     (vel_y * (1 + vel_y) / 2) as usize
 }
 
-fn step(&(x, y, vel_x, vel_y): &(isize, isize, isize, isize)) -> (isize, isize, isize, isize) {
-    let x = x + vel_x;
-    let y = y + vel_y;
-    let vel_x = vel_x - vel_x.signum();
-    let vel_y = vel_y - 1;
-    (x, y, vel_x, vel_y)
-}
-
 fn part_b(data: &'static str) -> usize {
-    let (min_x, max_x, min_y, max_y) = parse(data);
-    let mut init_count = 0;
-    for (vel_x, vel_y) in (1..=max_x).cartesian_product(min_y..-min_y) {
-        for (x, y, _vel_x, _vel_y) in iterate((0, 0, vel_x, vel_y), step) {
-            if x >= min_x && x <= max_x && y >= min_y && y <= max_y {
-                init_count += 1;
-                break;
-            }
-            if x > max_x || y < min_y {
-                break;
-            }
-        }
-    }
-    init_count
+    let [min_x, max_x, min_y, max_y] = parse(data);
+    let possible_vels_x = 1..=max_x;
+    let possible_vels_y = min_y..-min_y;
+    (possible_vels_x)
+        .cartesian_product(possible_vels_y)
+        .filter(|&(vel_x, vel_y)| {
+            iterate((0, 0, vel_x, vel_y), |&(x, y, vel_x, vel_y)| {
+                (x + vel_x, y + vel_y, vel_x - vel_x.signum(), vel_y - 1)
+            })
+            .take_while(|&(x, y, _, _)| x <= max_x && y >= min_y)
+            .any(|(x, y, _, _)| min_x <= x && x <= max_x && min_y <= y && y <= max_y)
+        })
+        .count()
 }
 
 #[cfg(test)]
