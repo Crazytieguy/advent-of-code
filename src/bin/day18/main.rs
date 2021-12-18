44ede8f965aeb89b789f75d2b48367750c8200c9
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-18 18:54:34 +0200

    refactoring

--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -1,5 +1,6 @@
 const DATA: &str = include_str!("data.txt");
 
+use derive_new::new;
 use itertools::Itertools;
 
 fn main() {
@@ -7,7 +8,7 @@ fn main() {
     println!("part b: {}", part_b(DATA));
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, new)]
 struct Element {
     open_before: usize,
     closed_before: usize,
@@ -19,22 +20,16 @@ fn parse(data: &'static str) -> Vec<Vec<Element>> {
 }
 
 fn parse_number(number: &'static str) -> Vec<Element> {
-    let groups = number.chars().group_by(|c| c.is_ascii_digit());
-    groups
-        .into_iter()
-        .map(|(_, group)| group)
-        .tuples()
-        .map(|(non_digits, digits)| {
-            let counts = non_digits.counts();
-            Element {
-                open_before: *counts.get(&'[').unwrap_or(&0),
-                closed_before: *counts.get(&']').unwrap_or(&0),
-                val: digits
-                    .exactly_one()
-                    .ok()
-                    .and_then(|c| c.to_digit(10))
-                    .unwrap() as usize,
-            }
+    number
+        .split_inclusive(|c: char| c.is_ascii_digit())
+        .flat_map(|elem| {
+            elem.chars().last().and_then(|c| c.to_digit(10)).map(|val| {
+                Element::new(
+                    elem.chars().filter(|&c| c == '[').count(),
+                    elem.chars().filter(|&c| c == ']').count(),
+                    val as usize,
+                )
+            })
         })
         .collect()
 }
@@ -51,16 +46,16 @@ fn reduce_snail_number(number: &mut Vec<Element>) {
         })
         .map(|(i, _)| i);
     if let Some(i) = explode_at {
-        if i > 0 {
-            number[i - 1].val += number[i].val;
-        }
-        if i + 2 < number.len() {
-            number[i + 2].val += number[i + 1].val;
-            number[i + 2].closed_before -= 1;
+        number[i].open_before -= 1;
+        if let Some(before_i) = i.checked_sub(1) {
+            number[before_i].val += number[i].val;
         }
         number[i].val = 0;
-        number[i].open_before -= 1;
-        number.remove(i + 1);
+        let removed = number.remove(i + 1);
+        if let Some(after) = number.get_mut(i + 1) {
+            after.val += removed.val;
+            after.closed_before -= 1
+        }
         reduce_snail_number(number);
         return;
     }
@@ -70,20 +65,13 @@ fn reduce_snail_number(number: &mut Vec<Element>) {
         .find_position(|elem| elem.val >= 10)
         .map(|(i, _)| i);
     if let Some(i) = split_at {
-        let cur_val = number[i].val;
-        number[i].val /= 2;
-        number.insert(
-            i + 1,
-            Element {
-                open_before: 0,
-                closed_before: 0,
-                val: cur_val / 2 + cur_val % 2,
-            },
-        );
         number[i].open_before += 1;
-        if i + 2 < number.len() {
-            number[i + 2].closed_before += 1;
+        if let Some(after) = number.get_mut(i + 1) {
+            after.closed_before += 1
         }
+        let new_val = number[i].val / 2 + number[i].val % 2;
+        number.insert(i + 1, Element::new(0, 0, new_val));
+        number[i].val /= 2;
         reduce_snail_number(number);
     }
 }
@@ -101,12 +89,6 @@ fn get_magnitude(number: &[Element]) -> usize {
     magnitude
 }
 
-fn part_a(data: &'static str) -> usize {
-    let numbers = parse(data);
-    let final_number = numbers.into_iter().reduce(sum_snail_numbers).unwrap();
-    get_magnitude(&final_number)
-}
-
 fn sum_snail_numbers(mut a: Vec<Element>, mut b: Vec<Element>) -> Vec<Element> {
     b[0].closed_before = a
         .iter()
@@ -117,17 +99,19 @@ fn sum_snail_numbers(mut a: Vec<Element>, mut b: Vec<Element>) -> Vec<Element> {
     a
 }
 
+fn part_a(data: &'static str) -> usize {
+    let numbers = parse(data);
+    let final_number = numbers.into_iter().reduce(sum_snail_numbers).unwrap();
+    get_magnitude(&final_number)
+}
+
 fn part_b(data: &'static str) -> usize {
     let numbers = parse(data);
     numbers
         .iter()
         .cartesian_product(numbers.iter())
         .flat_map(|(n0, n1)| [(n0, n1), (n1, n0)])
-        .map(|(n0, n1)| {
-            let mut summed = sum_snail_numbers(n0.clone(), n1.clone());
-            reduce_snail_number(&mut summed);
-            get_magnitude(&summed)
-        })
+        .map(|(n0, n1)| get_magnitude(&sum_snail_numbers(n0.clone(), n1.clone())))
         .max()
         .unwrap()
 }
@@ -142,21 +126,9 @@ mod tests {
         assert_eq!(
             parse_number("[[1,2],3]"),
             [
-                Element {
-                    open_before: 2,
-                    closed_before: 0,
-                    val: 1
-                },
-                Element {
-                    open_before: 0,
-                    closed_before: 0,
-                    val: 2
-                },
-                Element {
-                    open_before: 0,
-                    closed_before: 1,
-                    val: 3
-                },
+                Element::new(2, 0, 1),
+                Element::new(0, 0, 2),
+                Element::new(0, 1, 3),
             ]
         )
     }
@@ -173,28 +145,15 @@ mod tests {
 
     #[test]
     fn test_get_magnitue() {
-        assert_eq!(get_magnitude(&parse_number("[[1,2],[[3,4],5]]")), 143);
-        assert_eq!(
-            get_magnitude(&parse_number("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")),
-            1384
-        );
-        assert_eq!(
-            get_magnitude(&parse_number("[[[[1,1],[2,2]],[3,3]],[4,4]]")),
-            445
-        );
-        assert_eq!(
-            get_magnitude(&parse_number("[[[[3,0],[5,3]],[4,4]],[5,5]]")),
-            791
-        );
-        assert_eq!(
-            get_magnitude(&parse_number("[[[[5,0],[7,4]],[5,5]],[6,6]]")),
-            1137
-        );
-        assert_eq!(
-            get_magnitude(&parse_number(
-                "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
-            )),
-            3488
+        let test = |num, mag| assert_eq!(get_magnitude(&parse_number(num)), mag);
+        test("[[1,2],[[3,4],5]]", 143);
+        test("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384);
+        test("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445);
+        test("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791);
+        test("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137);
+        test(
+            "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]",
+            3488,
         );
     }
 
