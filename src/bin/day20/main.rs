d2cb2b2d06d7397daaa81efde69bc086293b9129
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-21 03:45:37 +0200

    rewrite using ndarray library

--- src/bin/day20/main.rs
+++ src/bin/day20/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
-
+#![allow(clippy::reversed_empty_ranges)]
 use derive_new::new;
-use itertools::{iterate, repeat_n, Itertools};
+use itertools::{iterate, Itertools};
+use ndarray::{array, s, Array, Array2, Zip};
 
 const DATA: &str = include_str!("data.txt");
 
@@ -12,68 +12,47 @@ fn main() {
 
 #[derive(new)]
 struct Image {
-    data: HashMap<(i64, i64), bool>,
+    data: Array2<bool>,
     default: bool,
-    bounds: ((i64, i64), (i64, i64)),
 }
 
 fn parse(data: &'static str) -> (Vec<bool>, Image) {
     let lines = data.lines().collect_vec();
     let image_enhancement = lines[0].chars().map(|c| c == '#').collect_vec();
-    let data = lines[2..]
+    let (rows, cols) = (lines[2..].len(), lines[2].len());
+    let default = false;
+    let mut data = Array2::from_elem([rows + 4, cols + 4], default);
+    let data_vec = lines[2..]
         .iter()
-        .enumerate()
-        .flat_map(|(row, line)| {
-            line.chars()
-                .enumerate()
-                .map(move |(col, c)| ((row as i64, col as i64), c == '#'))
-        })
-        .collect();
-    let max_bound = (lines[2..].len() as i64, lines[2].len() as i64);
-    (
-        image_enhancement,
-        Image::new(data, false, ((0, 0), max_bound)),
-    )
-}
-
-fn bits_to_number(bits: impl DoubleEndedIterator<Item = bool>) -> usize {
-    bits.rev().enumerate().map(|(i, b)| (b as usize) << i).sum()
+        .flat_map(|line| line.chars().map(|c| c == '#'))
+        .collect_vec();
+    data.slice_mut(s![2..-2, 2..-2])
+        .assign(&Array::from_shape_vec([rows, cols], data_vec).unwrap());
+    (image_enhancement, Image::new(data, default))
 }
 
 lazy_static::lazy_static! {
-    static ref ADJACENT_COORDS: [(i64, i64); 9] = (-1..=1).cartesian_product(-1..=1).collect_vec().try_into().unwrap();
+    static ref VAL_BY_POSITION: Array2<usize> = array![
+        [0b100000000, 0b010000000, 0b001000000],
+        [0b000100000, 0b000010000, 0b000001000],
+        [0b000000100, 0b000000010, 0b000000001],
+    ];
 }
 
 fn enhance_image(image_enhancement: &[bool], image: &Image) -> Image {
-    let ((min_row, min_col), (max_row, max_col)) = image.bounds;
-    let data = ((min_row - 1)..=(max_row + 1))
-        .cartesian_product((min_col - 1)..=(max_col + 1))
-        .map(|(row, col)| {
-            let bits = ADJACENT_COORDS.iter().map(|(d_row, d_col)| {
-                let coords = (row + d_row, col + d_col);
-                image.data.get(&coords).copied().unwrap_or(image.default)
-            });
-            let enhanced_bit = image_enhancement[bits_to_number(bits)];
-            ((row, col), enhanced_bit)
-        })
-        .collect();
-    let default = image_enhancement[bits_to_number(repeat_n(image.default, 9))];
-    let new_bounds = ((min_row - 1, min_col - 1), (max_row + 1, max_col + 1));
-    Image::new(data, default, new_bounds)
-}
-
-// for debugging
-#[allow(dead_code)]
-fn print_image(image: &Image) {
-    let ((min_row, min_col), (max_row, max_col)) = image.bounds;
-    let image_str: String = (min_row..=max_row)
-        .flat_map(|row| {
-            (min_col..=max_col)
-                .map(move |col| if image.data[&(row, col)] { '#' } else { '.' })
-                .chain(Some('\n'))
-        })
-        .collect();
-    println!("{}", image_str)
+    let shape = image.data.shape();
+    let default = image_enhancement[if image.default { 0b111111111 } else { 0 }];
+    let mut data = Array2::from_elem([shape[0] + 2, shape[1] + 2], default);
+    Zip::from(image.data.windows((3, 3))).map_assign_into(
+        data.slice_mut(s![2..-2, 2..-2]),
+        |window| {
+            let idx = Zip::from(window)
+                .and(&*VAL_BY_POSITION)
+                .fold(0, |acc, &is_bright, val| acc | (is_bright as usize * val));
+            image_enhancement[idx]
+        },
+    );
+    Image::new(data, default)
 }
 
 fn solve(data: &'static str, iterations: usize) -> usize {
@@ -81,7 +60,7 @@ fn solve(data: &'static str, iterations: usize) -> usize {
     let image = iterate(image, |image| enhance_image(&image_enhancement, image))
         .nth(iterations)
         .unwrap();
-    image.data.into_values().filter(|&b| b).count()
+    image.data.map(|&b| b as usize).sum()
 }
 
 fn part_a(data: &'static str) -> usize {
