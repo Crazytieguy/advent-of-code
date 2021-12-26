0ef8c395d6c4b82454f23ee659da43702aded3ca
Yoav Tzfati <yoav.tzfati@gmail.com>
2021-12-26 04:13:33 +0200

    day19 improvements

--- src/bin/day19/main.rs
+++ src/bin/day19/main.rs
@@ -1,13 +1,15 @@
-use std::{collections::HashMap, iter::repeat_with};
+use std::collections::HashSet;
 
 use glam::IVec3;
-use itertools::{iterate, Itertools};
+use itertools::Itertools;
 
 const DATA: &str = include_str!("data.txt");
 
 fn main() {
-    println!("part a: {}", part_a(DATA));
-    println!("part b: {}", part_b(DATA));
+    let unalligned = parse(DATA);
+    let (alligned, allignements) = allign_all_scanners(unalligned);
+    println!("part a: {}", num_beacons(alligned));
+    println!("part b: {}", greatest_manhatten_distance(allignements));
 }
 
 fn parse(data: &'static str) -> Vec<Vec<IVec3>> {
@@ -40,115 +42,110 @@ fn rotate_x(p: IVec3) -> IVec3 {
     IVec3::new(p.x, p.z, -p.y)
 }
 
-fn all_rotations(p: IVec3) -> impl Iterator<Item = IVec3> {
-    iterate(p, |&p| rotate_z(p))
-        .take(4)
-        .flat_map(|p| iterate(p, |&p| rotate_x(p)).take(4))
-        .flat_map(|p| iterate(p, |&p| rotate_y(p)).take(2))
-}
-
-fn all_scanner_variations(points: &[IVec3]) -> impl Iterator<Item = Vec<IVec3>> {
-    let mut iter_vec = points.iter().copied().map(all_rotations).collect_vec();
-    repeat_with(move || {
-        iter_vec
-            .iter_mut()
-            .map(|it| it.next())
-            .collect::<Option<Vec<IVec3>>>()
-    })
-    .while_some()
-}
-
-fn diffmap(points: &[IVec3]) -> HashMap<IVec3, Vec<(IVec3, IVec3)>> {
-    points
-        .iter()
-        .tuple_combinations()
-        .flat_map(|(&p1, &p2)| [(p1, p2), (p2, p1)])
-        .into_group_map_by(|&(p1, p2)| p2 - p1)
-}
-
-struct ScannerData {
-    points: Vec<IVec3>,
-    diffmap: HashMap<IVec3, Vec<(IVec3, IVec3)>>,
-}
-
-impl ScannerData {
-    fn new(points: Vec<IVec3>) -> Self {
-        let diffmap = diffmap(&points);
-        Self { points, diffmap }
+fn rotation(p: IVec3, n: usize) -> IVec3 {
+    match n {
+        0 => p,
+        1 => rotate_z(p),
+        2 => rotate_z(rotate_z(p)),
+        3 => rotate_z(rotate_z(rotate_z(p))),
+        4 => rotate_x(p),
+        5 => rotate_z(rotate_x(p)),
+        6 => rotate_z(rotate_z(rotate_x(p))),
+        7 => rotate_z(rotate_z(rotate_z(rotate_x(p)))),
+        8 => rotate_x(rotate_x(p)),
+        9 => rotate_z(rotate_x(rotate_x(p))),
+        10 => rotate_z(rotate_z(rotate_x(rotate_x(p)))),
+        11 => rotate_z(rotate_z(rotate_z(rotate_x(rotate_x(p))))),
+        12 => rotate_x(rotate_x(rotate_x(p))),
+        13 => rotate_z(rotate_x(rotate_x(rotate_x(p)))),
+        14 => rotate_z(rotate_z(rotate_x(rotate_x(rotate_x(p))))),
+        15 => rotate_z(rotate_z(rotate_z(rotate_x(rotate_x(rotate_x(p)))))),
+        16 => rotate_y(p),
+        17 => rotate_z(rotate_y(p)),
+        18 => rotate_z(rotate_z(rotate_y(p))),
+        19 => rotate_z(rotate_z(rotate_z(rotate_y(p)))),
+        20 => rotate_y(rotate_y(rotate_y(p))),
+        21 => rotate_z(rotate_y(rotate_y(rotate_y(p)))),
+        22 => rotate_z(rotate_z(rotate_y(rotate_y(rotate_y(p))))),
+        23 => rotate_z(rotate_z(rotate_z(rotate_y(rotate_y(rotate_y(p)))))),
+        _ => unreachable!(),
     }
 }
 
-fn get_alignement(a: &ScannerData, b: &ScannerData) -> Option<IVec3> {
-    a.diffmap
-        .iter()
-        .flat_map(|(&diff, pairs)| {
-            pairs
-                .iter()
-                .cartesian_product(b.diffmap.get(&diff).cloned().unwrap_or_default())
-        })
-        .flat_map(|(&(a1, a2), (b1, b2))| [(a1, b1), (a2, b2)])
-        .unique()
-        .map(|(a, b)| a - b)
-        .counts()
-        .into_iter()
-        .find(|(_, count)| *count >= 12)
-        .map(|(alignement, _)| alignement)
+fn all_rotations(p: IVec3) -> impl Iterator<Item = IVec3> {
+    (0..24).map(move |n| rotation(p, n))
 }
 
-fn allign_all_scanners(mut unalligned: Vec<Vec<IVec3>>) -> (Vec<Vec<IVec3>>, Vec<IVec3>) {
-    let mut alligned = vec![ScannerData::new(unalligned.remove(0))];
-    let mut allignements = vec![IVec3::new(0, 0, 0)];
-    let mut unalligned_all_variations = unalligned
-        .into_iter()
-        .map(|points| {
-            all_scanner_variations(&points)
-                .map(ScannerData::new)
-                .collect_vec()
-        })
+fn dist_squared((&p1, &p2): (&IVec3, &IVec3)) -> i32 {
+    let diff = p1 - p2;
+    diff.x * diff.x + diff.y * diff.y + diff.z * diff.z
+}
+
+fn distances_squared(scan: &[IVec3]) -> HashSet<i32> {
+    scan.iter().tuple_combinations().map(dist_squared).collect()
+}
+
+fn allign_all_scanners(unalligned: Vec<Vec<IVec3>>) -> (Vec<Vec<IVec3>>, Vec<IVec3>) {
+    let distances = unalligned
+        .iter()
+        .map(|s| distances_squared(s))
         .collect_vec();
-    while !unalligned_all_variations.is_empty() {
-        let ((scanner_idx, variation_idx), alignement) = alligned
+    let matches = distances
+        .iter()
+        .enumerate()
+        .tuple_combinations()
+        .filter(|((_, di), (_, dj))| di.intersection(dj).count() >= 66)
+        .flat_map(|((i, _), (j, _))| [(i, j), (j, i)])
+        .collect_vec();
+    let mut alligned: Vec<Vec<IVec3>> = vec![vec![]; unalligned.len()];
+    let mut allignements = vec![IVec3::new(0, 0, 0)];
+    alligned[0] = unalligned[0].clone();
+    while let Some(&(cur, next)) = matches
+        .iter()
+        .find(|&&(cur, next)| !alligned[cur].is_empty() && alligned[next].is_empty())
+    {
+        let common_distance = *distances[cur]
+            .intersection(&distances[next])
+            .next()
+            .unwrap();
+        let (&p1_alligned, &p2_alligned) = alligned[cur]
             .iter()
-            .rev()
-            .flat_map(|match_against| {
-                unalligned_all_variations.iter().enumerate().flat_map(
-                    move |(scanner_idx, scanner_variations)| {
-                        scanner_variations.iter().enumerate().map(
-                            move |(variation_idx, scannerdata)| {
-                                (match_against, scanner_idx, variation_idx, scannerdata)
-                            },
-                        )
-                    },
-                )
-            })
-            .find_map(|(match_against, scanner_idx, variation_idx, scannerdata)| {
-                Some((scanner_idx, variation_idx)).zip(get_alignement(match_against, scannerdata))
+            .tuple_combinations()
+            .find(|&pair| dist_squared(pair) == common_distance)
+            .unwrap();
+        let (&p1_unalligned, &p2_unalligned) = unalligned[next]
+            .iter()
+            .tuple_combinations()
+            .find(|&pair| dist_squared(pair) == common_distance)
+            .unwrap();
+        let (n, (p1_corrected, p2_corrected)) = all_rotations(p1_unalligned)
+            .zip(all_rotations(p2_unalligned))
+            .find_position(|&(candidate1, candidate2)| {
+                (candidate1 - p1_alligned == candidate2 - p2_alligned)
+                    || (candidate2 - p1_alligned == candidate1 - p2_alligned)
             })
             .unwrap();
-        alligned.push(ScannerData::new(
-            unalligned_all_variations.remove(scanner_idx)[variation_idx]
-                .points
-                .iter()
-                .map(|&p| p + alignement)
-                .collect(),
-        ));
-        allignements.push(alignement);
+        let allignement = if p1_alligned - p1_corrected == p2_alligned - p2_corrected {
+            p1_alligned - p1_corrected
+        } else {
+            assert_eq!(p2_alligned - p1_corrected, p1_alligned - p2_corrected);
+            p2_alligned - p1_corrected
+        };
+        allignements.push(allignement);
+        let new_alligned = unalligned[next]
+            .iter()
+            .map(|&p| rotation(p, n) + allignement)
+            .collect();
+        alligned[next] = new_alligned;
     }
-    (
-        alligned.into_iter().map(|sd| sd.points).collect(),
-        allignements,
-    )
+    (alligned, allignements)
 }
 
-fn part_a(data: &'static str) -> usize {
-    let unalligned = parse(data);
-    let (alligned, _) = allign_all_scanners(unalligned);
+fn num_beacons(alligned: Vec<Vec<IVec3>>) -> usize {
     alligned.into_iter().flatten().unique().count()
 }
 
-fn part_b(data: &'static str) -> usize {
-    let unalligned = parse(data);
-    let (_, allignements) = allign_all_scanners(unalligned);
+fn greatest_manhatten_distance(allignements: Vec<IVec3>) -> usize {
     allignements
         .into_iter()
         .tuple_combinations()
@@ -171,18 +168,16 @@ mod tests {
     }
 
     #[test]
-    fn test_all_scanner_variations() {
-        let scanner = parse(SAMPLE_DATA).remove(0);
-        assert_eq!(all_scanner_variations(&scanner).unique().count(), 24)
-    }
-
-    #[test]
     fn test_a() {
-        assert_eq!(part_a(SAMPLE_DATA), 79);
+        let unalligned = parse(SAMPLE_DATA);
+        let (alligned, _) = allign_all_scanners(unalligned);
+        assert_eq!(num_beacons(alligned), 79);
     }
 
     #[test]
     fn test_b() {
-        assert_eq!(part_b(SAMPLE_DATA), 3621);
+        let unalligned = parse(SAMPLE_DATA);
+        let (_, allignements) = allign_all_scanners(unalligned);
+        assert_eq!(greatest_manhatten_distance(allignements), 3621);
     }
 }
