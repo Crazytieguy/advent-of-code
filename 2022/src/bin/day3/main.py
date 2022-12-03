a6e23923a94ef68de2bf2078556dc5554f509b6b
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-03 08:39:54 +0200

    refactor python

--- 2022/src/bin/day3/main.py
+++ 2022/src/bin/day3/main.py
@@ -1,30 +1,43 @@
 import string
+from collections.abc import Iterable
 from pathlib import Path
-
-data = (Path(__file__).parent / "data.txt").read_text()
 
 
 def priority(item: str) -> int:
     return string.ascii_letters.index(item) + 1
 
 
-part_a = 0
-for line in data.splitlines():
-    left = line[: len(line) // 2]
-    right = line[len(line) // 2 :]
-    intersection = set(left) & set(right)
-    part_a += priority(intersection.pop())
+def find_single_intersection(group: Iterable[str]) -> str:
+    intersection = set.intersection(*(set(items) for items in group))
+    assert len(intersection) == 1, group
+    return intersection.pop()
 
 
-print(f"{part_a=}")
+def part_a(lines: list[str]) -> int:
+    ans = 0
+    for line in lines:
+        left = line[: len(line) // 2]
+        right = line[len(line) // 2 :]
+        single_intersection = find_single_intersection([left, right])
+        ans += priority(single_intersection)
+    return ans
 
-part_b = 0
-lines = data.splitlines()
-i = 0
-while i < len(lines):
-    group = lines[i : i + 3]
-    intersection = set.intersection(*(set(line) for line in group))
-    part_b += priority(intersection.pop())
-    i += 3
 
-print(f"{part_b=}")
+def part_b(lines: list[str]) -> int:
+    ans = 0
+    for i in range(0, len(lines), 3):
+        group = lines[i : i + 3]
+        single_intersection = find_single_intersection(group)
+        ans += priority(single_intersection)
+    return ans
+
+
+def main():
+    data = (Path(__file__).parent / "data.txt").read_text()
+    lines = data.splitlines()
+    print(f"part a: {part_a(lines)}")
+    print(f"part b: {part_b(lines)}")
+
+
+if __name__ == "__main__":
+    main()
