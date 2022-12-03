239ebe11b2e1d418ca83a71a57027f356dee20b0
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-03 07:39:00 +0200

    refactor day3

--- 2022/src/bin/day3/main.rs
+++ 2022/src/bin/day3/main.rs
@@ -1,54 +1,47 @@
-#![feature(array_chunks)]
-
 use itertools::Itertools;
 use std::collections::HashSet;
 
 const DATA: &str = include_str!("data.txt");
 
-type Parsed = Vec<&'static str>;
-
-fn parse(data: &'static str) -> Parsed {
-    data.lines().collect()
-}
-
-fn priority(c: char) -> usize {
-    match c {
-        'a'..='z' => c as usize - 'a' as usize + 1,
-        'A'..='Z' => c as usize - 'A' as usize + 27,
-        _ => unreachable!(),
+fn priority(item: char) -> usize {
+    match item {
+        'a'..='z' => item as usize - 'a' as usize + 1,
+        'A'..='Z' => item as usize - 'A' as usize + 27,
+        _ => panic!("illegal character {item}"),
     }
 }
 
-fn part_a(data: &Parsed) -> usize {
-    data.iter()
+fn char_set(items: &'static str) -> HashSet<char> {
+    items.chars().collect()
+}
+
+fn intersecting_item(group: impl Iterator<Item = &'static str>) -> char {
+    group
+        .map(char_set)
+        .reduce(|a, b| a.intersection(&b).copied().collect())
+        .expect("there to be more than one set")
+        .into_iter()
+        .exactly_one()
+        .expect("there is exactly one item in the intersection")
+}
+
+fn part_a(data: &'static str) -> usize {
+    data.lines()
         .map(|line| {
-            let (left, right) = (&line[..line.len() / 2], &line[line.len() / 2..]);
-            let intersection = left
-                .chars()
-                .collect::<HashSet<_>>()
-                .intersection(&right.chars().collect())
-                .copied()
-                .exactly_one()
-                .unwrap();
-            priority(intersection)
+            let (left, right) = line.split_at(line.len() / 2);
+            let item = intersecting_item([left, right].into_iter());
+            priority(item)
         })
         .sum()
 }
 
-fn part_b(data: &Parsed) -> usize {
-    data.array_chunks::<3>()
-        .map(|[first, second, thid]| {
-            let mut intersection = first.chars().collect::<HashSet<_>>();
-            intersection = intersection
-                .intersection(&second.chars().collect())
-                .copied()
-                .collect();
-            let l = intersection
-                .intersection(&thid.chars().collect())
-                .copied()
-                .exactly_one()
-                .unwrap();
-            priority(l)
+fn part_b(data: &'static str) -> usize {
+    data.lines()
+        .chunks(3)
+        .into_iter()
+        .map(|group| {
+            let item = intersecting_item(group);
+            priority(item)
         })
         .sum()
 }
@@ -60,19 +53,18 @@ mod tests {
 
     #[test]
     fn test_a() {
-        assert_eq!(part_a(&parse(SAMPLE_DATA)), 157);
-        println!("part a: {}", part_a(&parse(DATA)));
+        assert_eq!(part_a(SAMPLE_DATA), 157);
+        println!("part a: {}", part_a(DATA));
     }
 
     #[test]
     fn test_b() {
-        assert_eq!(part_b(&parse(SAMPLE_DATA)), 70);
-        println!("part b: {}", part_b(&parse(DATA)));
+        assert_eq!(part_b(SAMPLE_DATA), 70);
+        println!("part b: {}", part_b(DATA));
     }
 }
 
 fn main() {
-    let parsed = parse(DATA);
-    println!("part a: {}", part_a(&parsed));
-    println!("part b: {}", part_b(&parsed));
+    println!("part a: {}", part_a(DATA));
+    println!("part b: {}", part_b(DATA));
 }
