d77ec752a1074a55022597c1a1740845343d55c9
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-05 16:37:39 +0200

    day5 refactoring

--- 2022/src/bin/day5/main.rs
+++ 2022/src/bin/day5/main.rs
@@ -3,7 +3,7 @@ use nom::{
     bytes::complete::tag,
     character::complete::{char, line_ending, one_of, space0, u8},
     multi::{many1_count, separated_list1},
-    sequence::{delimited, pair, separated_pair, terminated, tuple},
+    sequence::{delimited, pair, separated_pair, tuple},
     IResult, Parser,
 };
 use std::error::Error;
@@ -12,10 +12,13 @@ const DATA: &str = include_str!("data.txt");
 
 struct Instruction {
     amount: u8,
-    from: usize,
-    to: usize,
+    from: u8,
+    to: u8,
 }
-type Parsed = (Vec<Vec<char>>, Vec<Instruction>);
+
+type Stacks = Vec<Vec<char>>;
+
+type Parsed = (Stacks, Vec<Instruction>);
 
 fn parse_crate(input: &str) -> IResult<&str, Option<char>> {
     alt((
@@ -32,48 +35,40 @@ fn parse_stack_numbers(input: &str) -> IResult<&str, usize> {
     many1_count(delimited(space0, u8, space0))(input)
 }
 
-fn parse_initial_state(input: &str) -> IResult<&str, Vec<Vec<char>>> {
-    let (input, (stacks, num_stacks)) = terminated(
-        separated_pair(
-            separated_list1(line_ending, parse_crates_row),
-            line_ending,
-            parse_stack_numbers,
-        ),
-        pair(line_ending, line_ending),
+fn parse_stacks(input: &str) -> IResult<&str, Stacks> {
+    let (input, (rows, num_stacks)) = separated_pair(
+        separated_list1(line_ending, parse_crates_row),
+        line_ending,
+        parse_stack_numbers,
     )(input)?;
-    let mut result = vec![vec![]; num_stacks];
-    for row in stacks {
-        for (i, crate_) in row.into_iter().enumerate() {
-            if let Some(c) = crate_ {
-                result[i].push(c);
+    let mut stacks = vec![vec![]; num_stacks];
+    for row in rows {
+        for (slot, stack) in row.into_iter().zip(stacks.iter_mut()) {
+            if let Some(crate_) = slot {
+                stack.push(crate_)
             }
         }
     }
-    result.iter_mut().for_each(|s| s.reverse());
-    Ok((input, result))
+    stacks.iter_mut().for_each(|s| s.reverse());
+    Ok((input, stacks))
 }
 
 fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
     let (input, (_, amount, _, from, _, to)) =
         tuple((tag("move "), u8, tag(" from "), u8, tag(" to "), u8))(input)?;
-    Ok((
-        input,
-        Instruction {
-            amount,
-            from: from as usize - 1,
-            to: to as usize - 1,
-        },
-    ))
+    let (from, to) = (from - 1, to - 1);
+    Ok((input, Instruction { amount, from, to }))
 }
 
 fn parse(data: &'static str) -> IResult<&'static str, Parsed> {
-    pair(
-        parse_initial_state,
+    separated_pair(
+        parse_stacks,
+        pair(line_ending, line_ending),
         separated_list1(line_ending, parse_instruction),
     )(data)
 }
 
-fn get_top_crates(stacks: Vec<Vec<char>>) -> String {
+fn get_top_crates(stacks: Stacks) -> String {
     stacks
         .into_iter()
         .map(|stack| {
@@ -84,30 +79,29 @@ fn get_top_crates(stacks: Vec<Vec<char>>) -> String {
         .collect()
 }
 
-fn move_crates(stacks: &mut [Vec<char>], instructions: &[Instruction], reverse: bool) {
+fn solve(stacks: &Stacks, instructions: &[Instruction], reverse_when_moving: bool) -> String {
+    let mut stacks = stacks.clone();
     let mut temp_stack = vec![];
-    for instruction in instructions {
-        let range = stacks[instruction.from].len() - instruction.amount as usize..;
-        let iter_crates = stacks[instruction.from].drain(range);
-        if reverse {
-            temp_stack.extend(iter_crates.rev());
+    for &Instruction { amount, from, to } in instructions {
+        let (amount, from, to) = (amount as usize, from as usize, to as usize);
+        let range = stacks[from].len() - amount..;
+        let crates_to_move = stacks[from].drain(range);
+        if reverse_when_moving {
+            temp_stack.extend(crates_to_move.rev());
         } else {
-            temp_stack.extend(iter_crates);
+            temp_stack.extend(crates_to_move);
         }
-        stacks[instruction.to].append(&mut temp_stack);
+        stacks[to].append(&mut temp_stack);
     }
+    get_top_crates(stacks)
 }
 
 fn part_a((stacks, instructions): &Parsed) -> String {
-    let mut stacks = stacks.clone();
-    move_crates(&mut stacks, instructions, true);
-    get_top_crates(stacks)
+    solve(stacks, instructions, true)
 }
 
 fn part_b((stacks, instructions): &Parsed) -> String {
-    let mut stacks = stacks.clone();
-    move_crates(&mut stacks, instructions, false);
-    get_top_crates(stacks)
+    solve(stacks, instructions, false)
 }
 
 #[cfg(test)]
