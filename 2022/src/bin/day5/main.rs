5e52b1fa67f5fba2dbe1cb7529c40fbd90917de2
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-06 09:57:13 +0200

    more readable parsing

--- 2022/src/bin/day5/main.rs
+++ 2022/src/bin/day5/main.rs
@@ -1,15 +1,19 @@
-use itertools::Itertools;
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_until},
-    character::complete::{char, line_ending, multispace0, one_of, u8},
-    multi::separated_list1,
+    bytes::complete::tag,
+    character::{
+        complete::{char, line_ending, one_of, u8},
+        streaming::not_line_ending,
+    },
+    multi::{many1_count, separated_list1},
     sequence::{delimited, separated_pair, tuple},
     Parser,
 };
-use std::error::Error;
+use nom_supreme::{multi::parse_separated_terminated, ParserExt};
+use std::{collections::VecDeque, error::Error};
 
 type OutResult = std::result::Result<(), Box<dyn Error>>;
+type IResult<'a, T> = nom::IResult<&'a str, T>;
 
 const DATA: &str = include_str!("data.txt");
 
@@ -19,54 +23,51 @@ struct Instruction {
     to: u8,
 }
 
-type Stacks = Vec<Vec<char>>;
-
+type Stacks = Vec<VecDeque<char>>;
 type Parsed = (Stacks, Vec<Instruction>);
 
-type IResult<'a, T> = nom::IResult<&'a str, T>;
-
-fn parse_crate(input: &str) -> IResult<Option<char>> {
-    alt((
-        delimited(char('['), one_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), char(']')).map(Some),
-        tag("   ").map(|_| None),
-    ))(input)
+fn crate_(input: &str) -> IResult<Option<char>> {
+    const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let crate_ = delimited(char('['), one_of(UPPERCASE), char(']')).map(Some);
+    let not_crate = tag("   ").value(None);
+    alt((crate_, not_crate))(input)
 }
 
-fn parse_stacks(input: &str) -> IResult<Stacks> {
-    let (input, initial_state) = take_until("\n\n")(input)?;
+fn stacks(input: &str) -> IResult<Stacks> {
     let mut stacks = vec![];
-    for mut line in initial_state.lines().dropping_back(1) {
-        for i in 0.. {
-            let (rest, c) = parse_crate(line)?;
-            if stacks.len() == i {
-                stacks.push(vec![]);
+    let crate_row = parse_separated_terminated(
+        crate_,
+        char(' '),
+        line_ending,
+        || 0,
+        |i, opt_crate| {
+            if i == stacks.len() {
+                stacks.push(VecDeque::new());
             }
-            if let Some(c) = c {
-                stacks[i].push(c);
+            if let Some(c) = opt_crate {
+                stacks[i].push_front(c);
             }
-            if rest.is_empty() {
-                break;
-            }
-            (line, _) = char(' ')(rest)?;
-        }
-    }
-    stacks.iter_mut().for_each(|s| s.reverse());
+            i + 1
+        },
+    );
+    let (input, _) = many1_count(crate_row)(input)?;
+    let (input, _) = not_line_ending.terminated(line_ending).parse(input)?;
     Ok((input, stacks))
 }
 
-fn parse_instruction(input: &str) -> IResult<Instruction> {
+fn instruction(input: &str) -> IResult<Instruction> {
     let (input, (_, amount, _, from, _, to)) =
         tuple((tag("move "), u8, tag(" from "), u8, tag(" to "), u8))(input)?;
     let (from, to) = (from - 1, to - 1);
     Ok((input, Instruction { amount, from, to }))
 }
 
+fn instructions(input: &str) -> IResult<Vec<Instruction>> {
+    separated_list1(line_ending, instruction)(input)
+}
+
 fn parse(data: &str) -> IResult<Parsed> {
-    separated_pair(
-        parse_stacks,
-        multispace0,
-        separated_list1(line_ending, parse_instruction),
-    )(data)
+    separated_pair(stacks, line_ending, instructions)(data)
 }
 
 fn get_top_crates(stacks: Stacks) -> String {
@@ -74,7 +75,7 @@ fn get_top_crates(stacks: Stacks) -> String {
         .into_iter()
         .map(|stack| {
             *stack
-                .last()
+                .back()
                 .expect("each stack should have at least one crate")
         })
         .collect()
@@ -82,7 +83,7 @@ fn get_top_crates(stacks: Stacks) -> String {
 
 fn solve(stacks: &Stacks, instructions: &[Instruction], reverse_order: bool) -> String {
     let mut stacks = stacks.clone();
-    let mut temp_stack = vec![];
+    let mut temp_stack = VecDeque::new();
     for &Instruction { amount, from, to } in instructions {
         let (amount, from, to) = (amount as usize, from as usize, to as usize);
         let at = stacks[from].len() - amount;
