9f8d58772e5f0a222b7fbef9f42d5c84aee9be14
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-08 14:01:13 +0200

    refactor

--- 2022/src/bin/day8/main.rs
+++ 2022/src/bin/day8/main.rs
@@ -8,34 +8,50 @@ fn parse(data: &str) -> Vec<&[u8]> {
     data.lines().map(|line| line.as_bytes()).collect()
 }
 
+fn find_visible_trees(
+    visible: &mut HashSet<(usize, usize)>,
+    data: impl Iterator<Item = (usize, impl Iterator<Item = (usize, u8)>)>,
+    row_length: usize,
+) {
+    let mut max_height_vertical = vec![0; row_length];
+    for (i, row) in data {
+        let mut max_height_horizontal = 0;
+        for (j, tree) in row {
+            if tree > max_height_horizontal || tree > max_height_vertical[j] {
+                visible.insert((i, j));
+            }
+            max_height_horizontal = max_height_horizontal.max(tree);
+            max_height_vertical[j] = max_height_vertical[j].max(tree);
+        }
+    }
+}
+
 fn part_a(data: &[&[u8]]) -> usize {
     let mut visible = HashSet::new();
-    let mut max_height_from_top = vec![0; data[0].len()];
-    data.iter().enumerate().for_each(|(i, row)| {
-        let mut max_height_from_left = 0;
-        row.iter().enumerate().for_each(|(j, &tree)| {
-            if tree > max_height_from_left || tree > max_height_from_top[j] {
-                visible.insert((i, j));
-            }
-            max_height_from_left = max_height_from_left.max(tree);
-            max_height_from_top[j] = max_height_from_top[j].max(tree);
-        });
-    });
-    let mut max_height_from_bottom = vec![0; data[0].len()];
-    data.iter().enumerate().rev().for_each(|(i, row)| {
-        let mut max_height_from_right = 0;
-        row.iter().enumerate().rev().for_each(|(j, &tree)| {
-            if tree > max_height_from_right || tree > max_height_from_bottom[j] {
-                visible.insert((i, j));
-            }
-            max_height_from_right = max_height_from_right.max(tree);
-            max_height_from_bottom[j] = max_height_from_bottom[j].max(tree);
-        });
-    });
+
+    // iterate from top left
+    find_visible_trees(
+        &mut visible,
+        data.iter()
+            .map(|row| row.iter().copied().enumerate())
+            .enumerate(),
+        data[0].len(),
+    );
+
+    // iterate from bottom right
+    find_visible_trees(
+        &mut visible,
+        data.iter()
+            .map(|row| row.iter().copied().enumerate().rev())
+            .enumerate()
+            .rev(),
+        data[0].len(),
+    );
+
     visible.len()
 }
 
-fn count_visible_trees(mut trees: impl ExactSizeIterator<Item = u8>, tree: u8) -> usize {
+fn count_visible_trees_from(mut trees: impl ExactSizeIterator<Item = u8>, tree: u8) -> usize {
     let num_trees = trees.len();
     trees.position(|t| t >= tree).map_or(num_trees, |p| p + 1)
 }
@@ -46,13 +62,13 @@ fn part_b(data: &[&[u8]]) -> usize {
         .flat_map(|(i, row)| {
             row.iter().enumerate().map(move |(j, &tree)| {
                 // to the right
-                count_visible_trees(row[j + 1..].iter().copied(), tree)
+                count_visible_trees_from(row[j + 1..].iter().copied(), tree)
                 // to the left
-                * count_visible_trees(row[..j].iter().rev().copied(), tree)
+                * count_visible_trees_from(row[..j].iter().rev().copied(), tree)
                 // to the bottom
-                * count_visible_trees(data[i + 1..].iter().map(|row| row[j]), tree)
+                * count_visible_trees_from(data[i + 1..].iter().map(|row| row[j]), tree)
                 // to the top
-                * count_visible_trees(data[..i].iter().rev().map(|row| row[j]), tree)
+                * count_visible_trees_from(data[..i].iter().rev().map(|row| row[j]), tree)
             })
         })
         .max()
