177d6229f042b171cb1250d13fc74b8b6bfe6178
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-11 11:57:30 +0200

    refactor

--- 2022/src/bin/day11/main.rs
+++ 2022/src/bin/day11/main.rs
@@ -27,7 +27,7 @@ enum Op {
 
 #[derive(Debug, Clone)]
 struct Monkey {
-    inspected: usize,
+    inspected: u64,
     items: VecDeque<u64>,
     operation: Op,
     test: u64,
@@ -38,14 +38,13 @@ struct Monkey {
 fn monkey_number(data: &str) -> IResult<usize> {
     tag("Monkey ")
         .precedes(u8.map(|n| n as usize))
-        .terminated(char(':'))
-        .terminated(line_ending)
+        .terminated(char(':').and(line_ending))
         .parse(data)
 }
 
 fn starting_items(data: &str) -> IResult<VecDeque<u64>> {
     tag("  Starting items: ")
-        .precedes(separated_list0(tag(", "), u64).map(Vec::into))
+        .precedes(separated_list0(tag(", "), u64).map(Into::into))
         .terminated(line_ending)
         .parse(data)
 }
@@ -82,33 +81,36 @@ fn if_false(data: &str) -> IResult<usize> {
         .parse(data)
 }
 
+fn monkey(data: &str) -> IResult<Monkey> {
+    tuple((
+        monkey_number,
+        starting_items,
+        operation,
+        test,
+        if_true,
+        if_false,
+    ))
+    .map(|(_, items, operation, test, if_true, if_false)| Monkey {
+        inspected: 0,
+        items,
+        operation,
+        test,
+        if_true,
+        if_false,
+    })
+    .parse(data)
+}
+
 fn parse(data: &str) -> IResult<Parsed> {
-    separated_list1(
-        line_ending,
-        tuple((
-            monkey_number,
-            starting_items,
-            operation,
-            test,
-            if_true,
-            if_false,
-        ))
-        .map(
-            |(_number, items, operation, test, if_true, if_false)| Monkey {
-                inspected: 0,
-                items,
-                operation,
-                test,
-                if_true,
-                if_false,
-            },
-        ),
-    )(data)
+    separated_list1(line_ending, monkey)(data)
 }
 
-fn part_a(data: &Parsed) -> usize {
-    let mut monkeys = data.clone();
-    for (_, turn) in (0..20).cartesian_product(0..monkeys.len()) {
+fn stuff_slinging_simian_shenanigans(
+    monkeys: &mut [Monkey],
+    rounds: usize,
+    manage_worry_level: impl Fn(u64) -> u64,
+) {
+    for (_, turn) in (0..rounds).cartesian_product(0..monkeys.len()) {
         while let Some(item) = monkeys[turn].items.pop_front() {
             monkeys[turn].inspected += 1;
             let new = match monkeys[turn].operation {
@@ -116,7 +118,7 @@ fn part_a(data: &Parsed) -> usize {
                 Op::Multiply(n) => item * n,
                 Op::Square => item * item,
             };
-            let new = new / 3;
+            let new = manage_worry_level(new);
             let throw_to = if new % monkeys[turn].test == 0 {
                 monkeys[turn].if_true
             } else {
@@ -125,6 +127,9 @@ fn part_a(data: &Parsed) -> usize {
             monkeys[throw_to].items.push_back(new);
         }
     }
+}
+
+fn monkey_business(monkeys: Vec<Monkey>) -> u64 {
     monkeys
         .into_iter()
         .map(|m| m.inspected)
@@ -133,32 +138,17 @@ fn part_a(data: &Parsed) -> usize {
         .product()
 }
 
-fn part_b(data: &Parsed) -> usize {
+fn part_a(data: &Parsed) -> u64 {
     let mut monkeys = data.clone();
-    let common_factor: u64 = monkeys.iter().map(|m| m.test).product();
-    for (_, turn) in (0..10000).cartesian_product(0..monkeys.len()) {
-        while let Some(item) = monkeys[turn].items.pop_front() {
-            monkeys[turn].inspected += 1;
-            let new = match monkeys[turn].operation {
-                Op::Add(n) => item + n,
-                Op::Multiply(n) => item * n,
-                Op::Square => item * item,
-            };
-            let new = new % common_factor;
-            let throw_to = if new % monkeys[turn].test == 0 {
-                monkeys[turn].if_true
-            } else {
-                monkeys[turn].if_false
-            };
-            monkeys[throw_to].items.push_back(new);
-        }
-    }
-    monkeys
-        .into_iter()
-        .map(|m| m.inspected)
-        .sorted_by_key(|&n| Reverse(n))
-        .take(2)
-        .product()
+    stuff_slinging_simian_shenanigans(&mut monkeys, 20, |n| n / 3);
+    monkey_business(monkeys)
+}
+
+fn part_b(data: &Parsed) -> u64 {
+    let mut monkeys = data.clone();
+    let least_common_denominator: u64 = monkeys.iter().map(|m| m.test).product();
+    stuff_slinging_simian_shenanigans(&mut monkeys, 10000, |n| n % least_common_denominator);
+    monkey_business(monkeys)
 }
 
 #[cfg(test)]
