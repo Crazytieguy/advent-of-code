6247e78057f0c15477f16103f6f629ad294f8964
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-15 03:49:13 +0200

    refactor

--- 2022/src/bin/day14/main.rs
+++ 2022/src/bin/day14/main.rs
@@ -1,9 +1,8 @@
 use std::{collections::HashSet, error::Error};
 
+use itertools::process_results;
 use nom::{
-    bytes::complete::tag,
     character::complete::{char, u16, u8},
-    multi::separated_list1,
     sequence::separated_pair,
 };
 
@@ -18,80 +17,63 @@ fn parse_coords(data: &str) -> IResult<(u16, u8)> {
     separated_pair(u16, char(','), u8)(data)
 }
 
-fn parse_continuous_segments(data: &str) -> IResult<Vec<(u16, u8)>> {
-    separated_list1(tag(" -> "), parse_coords)(data)
-}
-
 fn parse(data: &str) -> IResult<Parsed> {
     let mut rocks = HashSet::new();
     for line in data.lines() {
-        parse_continuous_segments(line)?
-            .1
-            .into_iter()
-            .reduce(|(x1, y1), (x2, y2)| {
-                let (xmin, xmax) = (x1.min(x2), x1.max(x2));
-                let (ymin, ymax) = (y1.min(y2), y1.max(y2));
-                for x in xmin..=xmax {
-                    for y in ymin..=ymax {
+        process_results(line.split(" -> ").map(parse_coords), |it| {
+            it.map(|(_, xy)| xy).reduce(|(x1, y1), (x2, y2)| {
+                for x in x1.min(x2)..=x1.max(x2) {
+                    for y in y1.min(y2)..=y1.max(y2) {
                         rocks.insert((x, y));
                     }
                 }
                 (x2, y2)
-            });
+            })
+        })?;
     }
     Ok(("", rocks))
 }
 
-fn part_a(data: &Parsed) -> usize {
-    let mut full_coords = data.clone();
+fn find_resting_spot(taken_coords: &Parsed, max_y: u8) -> (u16, u8) {
+    let mut x = 500;
+    for y in 1..=max_y {
+        if let Some(new_x) = [x, x - 1, x + 1]
+            .iter()
+            .find(|&&x| !taken_coords.contains(&(x, y)))
+        {
+            x = *new_x;
+        } else {
+            return (x, y - 1);
+        }
+    }
+    (x, max_y)
+}
+
+fn solve(data: &Parsed, stop_condition: impl Fn(u8, u8) -> bool) -> usize {
     let num_rocks = data.len();
-    let &max_y = data
+    let max_y = data
         .iter()
         .map(|(_, y)| y)
         .max()
-        .expect("At least one rock");
-    'outer: loop {
-        let mut x = 500;
-        for y in 0..max_y {
-            if let Some(new_x) = [x, x - 1, x + 1]
-                .iter()
-                .find(|&&x| !full_coords.contains(&(x, y + 1)))
-            {
-                x = *new_x;
-            } else {
-                full_coords.insert((x, y));
-                continue 'outer;
-            }
+        .expect("At least one rock")
+        + 1;
+    let mut taken_coords = data.clone();
+    loop {
+        let (x, y) = find_resting_spot(&taken_coords, max_y);
+        if stop_condition(y, max_y) {
+            break;
         }
-        break;
+        taken_coords.insert((x, y));
     }
-    full_coords.len() - num_rocks
+    taken_coords.len() - num_rocks
+}
+
+fn part_a(data: &Parsed) -> usize {
+    solve(data, |y, max_y| y == max_y)
 }
 
 fn part_b(data: &Parsed) -> usize {
-    let mut full_coords = data.clone();
-    let num_rocks = data.len();
-    let &max_y = data
-        .iter()
-        .map(|(_, y)| y)
-        .max()
-        .expect("At least one rock");
-    'outer: while !full_coords.contains(&(500, 0)) {
-        let mut x = 500;
-        for y in 0..max_y + 2 {
-            if let Some(new_x) = [x, x - 1, x + 1]
-                .iter()
-                .find(|&&x| !full_coords.contains(&(x, y + 1)))
-            {
-                x = *new_x;
-            } else {
-                full_coords.insert((x, y));
-                continue 'outer;
-            }
-        }
-        full_coords.insert((x, max_y + 1));
-    }
-    full_coords.len() - num_rocks
+    solve(data, |y, _| y == 0) + 1
 }
 
 #[cfg(test)]
