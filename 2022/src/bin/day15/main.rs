55e78a8543e3a2b85a915313e187f6901b0fd3c6
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-16 19:36:59 +0200

    refactor 2

--- 2022/src/bin/day15/main.rs
+++ 2022/src/bin/day15/main.rs
@@ -1,27 +1,20 @@
-use std::{error::Error, ops::RangeInclusive};
-
 use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
     character::complete::{i32, line_ending},
     multi::separated_list1,
     sequence::tuple,
-    Parser,
 };
 use nom_supreme::ParserExt;
+use std::{error::Error, ops::Range};
 
 const DATA: &str = include_str!("data.txt");
 
 type OutResult = std::result::Result<(), Box<dyn Error>>;
 type IResult<'a, T> = nom::IResult<&'a str, T>;
 
+type Pair = (i32, i32, i32, i32);
 type Parsed = Vec<Pair>;
-
-#[derive(Debug, Clone, Copy)]
-struct Pair {
-    sensor: (i32, i32),
-    beacon: (i32, i32),
-}
 
 fn parse_pair(input: &str) -> IResult<Pair> {
     tuple((
@@ -29,63 +22,51 @@ fn parse_pair(input: &str) -> IResult<Pair> {
         tag(", y=").precedes(i32),
         tag(": closest beacon is at x=").precedes(i32),
         tag(", y=").precedes(i32),
-    ))
-    .map(|(sensor_x, sensor_y, beacon_x, beacon_y)| Pair {
-        sensor: (sensor_x, sensor_y),
-        beacon: (beacon_x, beacon_y),
-    })
-    .parse(input)
+    ))(input)
 }
 
 fn parse(data: &str) -> IResult<Parsed> {
     separated_list1(line_ending, parse_pair)(data)
 }
 
-impl Pair {
-    fn covered_xs(&self, row: i32) -> Option<RangeInclusive<i32>> {
-        let manhattan_distance =
-            (self.sensor.0 - self.beacon.0).abs() + (self.sensor.1 - self.beacon.1).abs();
-        let x_offset = manhattan_distance - (self.sensor.1 - row).abs();
-        Some(self.sensor.0 - x_offset..=self.sensor.0 + x_offset).filter(|r| !r.is_empty())
-    }
+fn covered_xs((sensor_x, sensor_y, beacon_x, beacon_y): Pair, row: i32) -> Option<Range<i32>> {
+    let manhattan_distance = (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs();
+    let x_offset = manhattan_distance - (sensor_y - row).abs();
+    Some(sensor_x - x_offset..sensor_x + x_offset + 1).filter(|r| !r.is_empty())
 }
 
-fn part_a(data: &Parsed, interesting_row: i32) -> i32 {
-    let count_covered_xs = data
+fn part_a(data: &Parsed, example_row: i32) -> usize {
+    let count_covered_xs: usize = data
         .iter()
-        .flat_map(|pair| pair.covered_xs(interesting_row))
-        .sorted_by_key(|covered_xs| *covered_xs.start())
-        .fold((0, i32::MIN), |(count, end), covered_xs| {
-            let count_from = (*covered_xs.start()).max(end + 1);
-            let added_count = 0.max(*covered_xs.end() + 1 - count_from);
-            (count + added_count, end.max(*covered_xs.end()))
+        .flat_map(|&pair| covered_xs(pair, example_row))
+        .sorted_unstable_by_key(|range| range.start)
+        .coalesce(|a, b| {
+            if a.end >= b.start {
+                Ok(a.start..b.end.max(a.end))
+            } else {
+                Err((a, b))
+            }
         })
-        .0;
+        .map(|xs| xs.len())
+        .sum();
     let blocked_xs = data
         .iter()
-        .flat_map(|&Pair { sensor, beacon }| [sensor, beacon])
-        .filter(|&(_, y)| y == interesting_row)
+        .flat_map(|&(sx, sy, bx, by)| [(sx, sy), (bx, by)])
+        .filter(|&(_, y)| y == example_row)
         .unique()
         .count();
-    count_covered_xs - blocked_xs as i32
+    count_covered_xs - blocked_xs
 }
 
 fn part_b(data: &Parsed, max_coord: i32) -> i64 {
-    let sorted_pairs = {
-        let mut pairs = data.clone();
-        pairs.sort_by_key(|pair| pair.sensor.0);
-        pairs
-    };
-
+    let sorted_pairs = data.iter().copied().sorted_unstable().collect_vec();
     for y in 0..=max_coord {
-        let mut x = 0;
-        for covered_xs in sorted_pairs.iter().flat_map(|pair| pair.covered_xs(y)) {
-            if covered_xs.contains(&x) {
-                x = covered_xs.end() + 1;
-            }
-        }
-        if x <= max_coord {
-            return x as i64 * 4000000 + y as i64;
+        let first_available_x = sorted_pairs
+            .iter()
+            .flat_map(|&pair| covered_xs(pair, y))
+            .fold(0, |x, range| if range.contains(&x) { range.end } else { x });
+        if first_available_x <= max_coord {
+            return first_available_x as i64 * 4_000_000 + y as i64;
         }
     }
     unreachable!("no solution found")
@@ -99,21 +80,21 @@ mod tests {
     #[test]
     fn test_a() -> OutResult {
         assert_eq!(part_a(&parse(SAMPLE_DATA)?.1, 10), 26);
-        println!("part a: {}", part_a(&parse(DATA)?.1, 2000000));
+        println!("part a: {}", part_a(&parse(DATA)?.1, 2_000_000));
         Ok(())
     }
 
     #[test]
     fn test_b() -> OutResult {
-        assert_eq!(part_b(&parse(SAMPLE_DATA)?.1, 20), 56000011);
-        println!("part b: {}", part_b(&parse(DATA)?.1, 4000000));
+        assert_eq!(part_b(&parse(SAMPLE_DATA)?.1, 20), 56_000_011);
+        println!("part b: {}", part_b(&parse(DATA)?.1, 4_000_000));
         Ok(())
     }
 }
 
 fn main() -> OutResult {
     let parsed = parse(DATA)?.1;
-    println!("part a: {}", part_a(&parsed, 2000000));
-    println!("part b: {}", part_b(&parsed, 4000000));
+    println!("part a: {}", part_a(&parsed, 2_000_000));
+    println!("part b: {}", part_b(&parsed, 4_000_000));
     Ok(())
 }
