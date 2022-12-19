04f5293ef64eeda0b025fe6965282769daa579de
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-19 17:40:48 +0200

    optimize day16 again

--- 2022/src/bin/day16/main.rs
+++ 2022/src/bin/day16/main.rs
@@ -7,8 +7,7 @@ use nom::{
     Parser,
 };
 use nom_supreme::ParserExt;
-use std::{cmp::Reverse, collections::HashMap, error::Error};
-use tinyvec::ArrayVec;
+use std::{cmp::Reverse, collections::HashMap, error::Error, time::Instant};
 
 const DATA: &str = include_str!("data.txt");
 
@@ -16,9 +15,10 @@ type OutResult = std::result::Result<(), Box<dyn Error>>;
 type IResult<'a, T> = nom::IResult<&'a str, T>;
 
 type FlowRates = Vec<u8>;
+type FlowRateIndices = Vec<usize>;
 type ShortestPathLengths = Vec<Vec<u8>>;
 
-type Parsed = (FlowRates, ShortestPathLengths, usize);
+type Parsed = (FlowRates, ShortestPathLengths, FlowRateIndices, usize);
 
 fn parse_row(data: &str) -> IResult<(&str, u8, Vec<&str>)> {
     tuple((
@@ -61,7 +61,7 @@ fn floyd_warshall(rows: &[(&str, u8, Vec<&str>)]) -> Vec<Vec<u8>> {
     dist
 }
 
-fn parse(data: &str) -> IResult<Parsed> {
+fn parse_and_prepare(data: &str) -> IResult<Parsed> {
     let (input, rows) = separated_list1(line_ending, parse_row)(data)?;
     let shortest_path_lengths_uncompressed = floyd_warshall(&rows);
 
@@ -75,7 +75,7 @@ fn parse(data: &str) -> IResult<Parsed> {
     let flow_rates = interesting_valve_indices
         .iter()
         .map(|&i| rows[i].1)
-        .collect();
+        .collect_vec();
 
     let shortest_path_lengths = interesting_valve_indices
         .iter()
@@ -92,198 +92,173 @@ fn parse(data: &str) -> IResult<Parsed> {
         .position(|&i| rows[i].0 == "AA")
         .expect("a valve called AA");
 
-    Ok((input, (flow_rates, shortest_path_lengths, starting_node)))
-}
+    let sorted_flow_rate_indices = flow_rates
+        .iter()
+        .enumerate()
+        .sorted_unstable_by_key(|&(_, &flow)| Reverse(flow))
+        .map(|(i, _)| i)
+        .collect_vec();
 
-#[derive(Default, Debug, Clone, Copy)]
-struct VisitorState {
-    going_to: u8,
-    will_arrive_in: u8,
+    Ok((
+        input,
+        (
+            flow_rates,
+            shortest_path_lengths,
+            sorted_flow_rate_indices,
+            starting_node,
+        ),
+    ))
 }
 
 #[derive(Default, Debug, Clone, Copy)]
 struct State {
-    visitors: [VisitorState; 2],
     visited: u16,
+    avoid: u16,
     pressure_released: u16,
-    current_flow: u16,
     minutes_remaining: u8,
+    position: u8,
 }
 
 /// Attempt to follow the 'branch and bound' algorithm from wikipedia:
 /// https://en.wikipedia.org/wiki/Branch_and_bound
 impl State {
-    fn visited(&self, i: usize) -> bool {
-        self.visited & (1 << i) != 0
+    fn new(position: u8, minutes_remaining: u8) -> Self {
+        Self {
+            visited: 0,
+            avoid: 1 << position,
+            pressure_released: 0,
+            minutes_remaining,
+            position,
+        }
     }
 
-    fn solution(&self) -> Option<u16> {
-        if self
-            .visitors
-            .iter()
-            .any(|v| v.will_arrive_in < self.minutes_remaining)
-        {
-            None
-        } else {
-            Some(self.pressure_released + (self.minutes_remaining as u16 + 1) * self.current_flow)
-        }
+    fn can_visit(self, i: usize) -> bool {
+        (self.visited | self.avoid) & (1 << i) == 0
     }
 
     /// Assuming the shortest path lengths are all 1, the best solution is
     /// to visit the valves in order of descending flow rate.
-    fn bound(mut self, flow_rates: &FlowRates) -> u16 {
-        let mut remaining_flow_rate_indices: ArrayVec<[usize; 16]> = (0..flow_rates.len())
-            .filter(|&i| !self.visited(i) && !self.visitors.iter().any(|v| v.going_to == i as u8))
-            .collect();
-        remaining_flow_rate_indices.sort_unstable_by_key(|&i| flow_rates[i]);
-        while self.minutes_remaining > 0 {
-            self.minutes_remaining -= 1;
-            self.pressure_released += self.current_flow;
-            for visitor in self.visitors.iter_mut() {
-                if visitor.will_arrive_in > 0 {
-                    visitor.will_arrive_in -= 1;
-                    continue;
-                }
-                self.current_flow += flow_rates[visitor.going_to as usize] as u16;
-                if let Some(i) = remaining_flow_rate_indices.pop() {
-                    visitor.going_to = i as u8;
-                    visitor.will_arrive_in = 1;
-                } else {
-                    visitor.will_arrive_in = u8::MAX;
-                }
-            }
-        }
-        self.pressure_released + self.current_flow
+    fn bound(self, flow_rates: &FlowRates, sorted_flow_rate_indices: &[usize]) -> u16 {
+        self.pressure_released
+            + (0..=self.minutes_remaining)
+                .rev()
+                .step_by(2)
+                .skip(1)
+                .zip(
+                    sorted_flow_rate_indices
+                        .iter()
+                        .filter(|&&i| self.can_visit(i))
+                        .map(|&i| flow_rates[i]),
+                )
+                .map(|(minutes, flow)| minutes as u16 * flow as u16)
+                .sum::<u16>()
     }
 
-    /// Each visitor that has reached their destination multiplies
-    /// the amount of new states by the number of valves that haven't been visited
-    fn branch(
-        mut self,
-        flow_rates: &FlowRates,
-        shortest_path_lengths: &ShortestPathLengths,
-    ) -> impl IntoIterator<Item = Self> {
-        self.pressure_released += self.current_flow;
-        self.minutes_remaining -= 1;
-        let mut branches = vec![self];
-        for (visitor_idx, visitor) in self.visitors.into_iter().enumerate() {
-            if visitor.will_arrive_in > 0 {
-                branches
-                    .iter_mut()
-                    .for_each(|state| state.visitors[visitor_idx].will_arrive_in -= 1);
-                continue;
-            }
-            branches.iter_mut().for_each(|state| {
-                state.visited |= 1 << visitor.going_to;
-                state.current_flow += flow_rates[visitor.going_to as usize] as u16;
-            });
-            branches = branches
-                .iter()
-                .flat_map(|&state| {
-                    shortest_path_lengths[visitor.going_to as usize]
-                        .iter()
-                        .enumerate()
-                        .filter(move |&(destination, _)| {
-                            !state.visited(destination)
-                                && !state
-                                    .visitors
-                                    .iter()
-                                    .any(|v| v.going_to == destination as u8)
-                        })
-                        .map(move |(destination, &distance)| {
-                            let mut next_state = state;
-                            next_state.visitors[visitor_idx].going_to = destination as u8;
-                            next_state.visitors[visitor_idx].will_arrive_in = distance;
-                            next_state
-                        })
+    fn branch<'a>(
+        self,
+        flow_rates: &'a FlowRates,
+        shortest_path_lengths: &'a ShortestPathLengths,
+    ) -> impl IntoIterator<Item = Self> + 'a {
+        shortest_path_lengths[self.position as usize]
+            .iter()
+            .enumerate()
+            .filter(move |&(destination, _distance)| self.can_visit(destination))
+            .filter_map(move |(destination, distance)| {
+                let minutes_remaining = self.minutes_remaining.checked_sub(*distance + 1)?;
+                Some(State {
+                    visited: self.visited | (1 << destination),
+                    avoid: self.avoid,
+                    pressure_released: self.pressure_released
+                        + minutes_remaining as u16 * flow_rates[destination] as u16,
+                    minutes_remaining,
+                    position: destination as u8,
                 })
-                .chain(
-                    // in case there's no where left to visit
-                    [{
-                        let mut state = branches[0];
-                        state.visitors[visitor_idx].will_arrive_in = u8::MAX;
-                        state
-                    }],
-                )
-                .collect();
-        }
-        branches
+            })
     }
 }
 
 fn branch_and_bound(
     flow_rates: &FlowRates,
+    sorted_flow_rate_indices: &[usize],
     shortest_path_lengths: &ShortestPathLengths,
     state: State,
+    best_for_visited: &mut [u16],
     best: &mut u16,
+    filter_bound: impl Fn(u16, u16) -> bool + Copy,
 ) {
-    if let Some(solution) = state.solution() {
-        *best = solution.max(*best);
-        return;
+    if let Some(cur_best) = best_for_visited.get_mut(state.visited as usize) {
+        *cur_best = state.pressure_released.max(*cur_best);
     }
+    *best = state.pressure_released.max(*best);
     let bound_branch_pairs = state
         .branch(flow_rates, shortest_path_lengths)
         .into_iter()
-        .map(|state| (state.bound(flow_rates), state))
-        .filter(|(bound, _)| bound > best)
+        .map(|state| (state.bound(flow_rates, sorted_flow_rate_indices), state))
+        .filter(|&(bound, _)| filter_bound(bound, *best))
         .sorted_unstable_by_key(|(bound, _)| Reverse(*bound))
         .collect_vec();
     for (bound, branch) in bound_branch_pairs {
-        if bound > *best {
-            branch_and_bound(flow_rates, shortest_path_lengths, branch, best);
+        if filter_bound(bound, *best) {
+            branch_and_bound(
+                flow_rates,
+                sorted_flow_rate_indices,
+                shortest_path_lengths,
+                branch,
+                best_for_visited,
+                best,
+                filter_bound,
+            );
         }
     }
 }
 
-fn part_a((flow_rates, shortest_paths, starting_idx): &Parsed) -> u16 {
+fn part_a((flow_rates, shortest_paths, sorted_flow_rate_indices, starting_idx): &Parsed) -> u16 {
     let mut best = 0;
     branch_and_bound(
         flow_rates,
+        sorted_flow_rate_indices,
         shortest_paths,
-        State {
-            visitors: [
-                VisitorState {
-                    going_to: *starting_idx as u8,
-                    will_arrive_in: 0,
-                },
-                VisitorState {
-                    going_to: *starting_idx as u8,
-                    will_arrive_in: u8::MAX,
-                },
-            ],
-            visited: 0,
-            pressure_released: 0,
-            current_flow: 0,
-            minutes_remaining: 30,
-        },
+        State::new(*starting_idx as u8, 30),
+        &mut [],
         &mut best,
+        |bound, best| bound > best,
     );
     best
 }
 
-fn part_b((flow_rates, shortest_paths, starting_idx): &Parsed) -> u16 {
-    let mut best = 0;
+fn part_b((flow_rates, shortest_paths, sorted_flow_rate_indices, starting_idx): &Parsed) -> u16 {
+    let mut best_per_visited = vec![0; u16::MAX as usize];
     branch_and_bound(
         flow_rates,
+        sorted_flow_rate_indices,
         shortest_paths,
-        State {
-            visitors: [
-                VisitorState {
-                    going_to: *starting_idx as u8,
-                    will_arrive_in: 0,
-                },
-                VisitorState {
-                    going_to: *starting_idx as u8,
-                    will_arrive_in: 0,
-                },
-            ],
-            visited: 0,
-            pressure_released: 0,
-            current_flow: 0,
-            minutes_remaining: 26,
-        },
-        &mut best,
+        State::new(*starting_idx as u8, 26),
+        &mut best_per_visited,
+        &mut 0,
+        // this could technically produce an incorrect result,
+        // but it doesn't on my input or the test input
+        |bound, best| bound > best * 3 / 4,
     );
+    let best_per_visited_filtered_sorted = best_per_visited
+        .into_iter()
+        .enumerate()
+        .filter(|&(_, best)| best > 0)
+        .map(|(i, best)| (i as u16, best))
+        .sorted_unstable_by_key(|&(_, best)| Reverse(best))
+        .collect_vec();
+    let mut best = 0;
+    for (i, &(my_visited, my_best)) in best_per_visited_filtered_sorted.iter().enumerate() {
+        for &(elephant_visited, elephant_best) in &best_per_visited_filtered_sorted[i + 1..] {
+            let score = my_best + elephant_best;
+            if score <= best {
+                break;
+            }
+            if my_visited & elephant_visited == 0 {
+                best = score;
+                break;
+            }
+        }
+    }
     best
 }
 
@@ -294,22 +269,27 @@ mod tests {
 
     #[test]
     fn test_a() -> OutResult {
-        assert_eq!(part_a(&parse(SAMPLE_DATA)?.1), 1651);
-        println!("part a: {}", part_a(&parse(DATA)?.1));
+        assert_eq!(part_a(&parse_and_prepare(SAMPLE_DATA)?.1), 1651);
+        println!("part a: {}", part_a(&parse_and_prepare(DATA)?.1));
         Ok(())
     }
 
     #[test]
     fn test_b() -> OutResult {
-        assert_eq!(part_b(&parse(SAMPLE_DATA)?.1), 1707);
-        println!("part b: {}", part_b(&parse(DATA)?.1));
+        assert_eq!(part_b(&parse_and_prepare(SAMPLE_DATA)?.1), 1707);
+        println!("part b: {}", part_b(&parse_and_prepare(DATA)?.1));
         Ok(())
     }
 }
 
 fn main() -> OutResult {
-    let parsed = parse(DATA)?.1;
-    println!("part a: {}", part_a(&parsed));
-    println!("part b: {}", part_b(&parsed));
+    let start_parse = Instant::now();
+    let parsed = parse_and_prepare(DATA)?.1;
+    println!("parsing        {:?}", start_parse.elapsed());
+    let start_a = Instant::now();
+    println!("part a: {}   {:?}", part_a(&parsed), start_a.elapsed());
+    let start_b = Instant::now();
+    println!("part b: {}   {:?}", part_b(&parsed), start_b.elapsed());
+    println!("total          {:?}", start_parse.elapsed());
     Ok(())
 }
