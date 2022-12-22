22a1400d93907e4e4a468f12567c10ec2c0e30ed
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-22 02:56:18 +0200

    refactor day21

--- 2022/src/bin/day21/main.rs
+++ 2022/src/bin/day21/main.rs
@@ -1,12 +1,15 @@
-use std::{collections::HashMap, error::Error};
-
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alpha1, i64},
-    sequence::separated_pair,
+    character::complete::{alpha1, i64, line_ending},
+    multi::separated_list1,
+    sequence::{separated_pair, tuple},
     Parser,
 };
+use nom_supreme::ParserExt;
+use std::{collections::HashMap, error::Error};
+use Expression::*;
+use Operator::*;
 
 const DATA: &str = include_str!("data.txt");
 
@@ -16,119 +19,124 @@ type IResult<'a, T> = nom::IResult<&'a str, T>;
 type Parsed<'a> = HashMap<&'a str, Expression<'a>>;
 
 #[derive(Debug, Clone, Copy)]
-enum Expression<'a> {
-    Number(i64),
-    Add(&'a str, &'a str),
-    Subtract(&'a str, &'a str),
-    Multiply(&'a str, &'a str),
-    Divide(&'a str, &'a str),
+enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
 }
 
-fn parse_expression(input: &str) -> IResult<Expression> {
+impl Operator {
+    fn eval(self, left: i64, right: i64) -> i64 {
+        match self {
+            Add => left + right,
+            Subtract => left - right,
+            Multiply => left * right,
+            Divide => left / right,
+        }
+    }
+}
+
+fn operator(input: &str) -> IResult<Operator> {
     alt((
-        i64.map(Expression::Number),
-        separated_pair(alpha1, tag(" + "), alpha1).map(|(a, b)| Expression::Add(a, b)),
-        separated_pair(alpha1, tag(" - "), alpha1).map(|(a, b)| Expression::Subtract(a, b)),
-        separated_pair(alpha1, tag(" * "), alpha1).map(|(a, b)| Expression::Multiply(a, b)),
-        separated_pair(alpha1, tag(" / "), alpha1).map(|(a, b)| Expression::Divide(a, b)),
+        tag(" + ").value(Add),
+        tag(" - ").value(Subtract),
+        tag(" * ").value(Multiply),
+        tag(" / ").value(Divide),
     ))(input)
 }
 
-fn parse_monkey(input: &str) -> IResult<(&str, Expression)> {
-    separated_pair(alpha1, tag(": "), parse_expression)(input)
+#[derive(Debug, Clone, Copy)]
+enum Expression<'a> {
+    Number(i64),
+    Operation((&'a str, Operator, &'a str)),
+}
+
+fn expression(input: &str) -> IResult<Expression> {
+    alt((
+        i64.map(Number),
+        tuple((alpha1, operator, alpha1)).map(Operation),
+    ))(input)
+}
+
+fn monkey(input: &str) -> IResult<(&str, Expression)> {
+    separated_pair(alpha1, tag(": "), expression)(input)
 }
 
 fn parse(data: &str) -> IResult<Parsed> {
-    let mut monkeys = HashMap::new();
-    for line in data.lines() {
-        let (_, (monkey, expr)) = parse_monkey(line)?;
-        monkeys.insert(monkey, expr);
-    }
-    Ok(("", monkeys))
+    separated_list1(line_ending, monkey)
+        .map(|monkeys| monkeys.into_iter().collect())
+        .parse(data)
 }
 
-fn eval<'a>(monkey: &'a str, monkeys: &'a Parsed, memo: &mut HashMap<&'a str, i64>) -> i64 {
-    if let Some(&val) = memo.get(monkey) {
-        return val;
+fn eval(monkeys: &Parsed, monkey: &str) -> i64 {
+    match monkeys[monkey] {
+        Number(n) => n,
+        Operation((left_monkey, op, right_monkey)) => {
+            op.eval(eval(monkeys, left_monkey), eval(monkeys, right_monkey))
+        }
     }
-    let val = match monkeys[monkey] {
-        Expression::Number(n) => n,
-        Expression::Add(a, b) => eval(a, monkeys, memo) + eval(b, monkeys, memo),
-        Expression::Subtract(a, b) => eval(a, monkeys, memo) - eval(b, monkeys, memo),
-        Expression::Multiply(a, b) => eval(a, monkeys, memo) * eval(b, monkeys, memo),
-        Expression::Divide(a, b) => eval(a, monkeys, memo) / eval(b, monkeys, memo),
-    };
-    memo.insert(monkey, val);
-    val
 }
 
 fn part_a(data: &Parsed) -> i64 {
-    eval("root", data, &mut HashMap::new())
+    eval(data, "root")
 }
 
-fn eval_b<'a>(
-    monkey: &'a str,
+impl Operator {
+    fn solve_for_left(self, result: i64, right: i64) -> i64 {
+        match self {
+            Add => result - right,
+            Subtract => result + right,
+            Multiply => result / right,
+            Divide => result * right,
+        }
+    }
+
+    fn solve_for_right(self, result: i64, left: i64) -> i64 {
+        match self {
+            Add => result - left,
+            Subtract => left - result,
+            Multiply => result / left,
+            Divide => left / result,
+        }
+    }
+}
+
+fn fill_knowns<'a>(
+    knowns: &mut HashMap<&'a str, i64>,
     monkeys: &'a Parsed,
-    memo: &mut HashMap<&'a str, i64>,
+    monkey: &'a str,
 ) -> Option<i64> {
     if monkey == "humn" {
         return None;
     }
     let val = match monkeys[monkey] {
-        Expression::Number(n) => n,
-        Expression::Add(a, b) => eval_b(a, monkeys, memo)
-            .zip(eval_b(b, monkeys, memo))
-            .map(|(a, b)| a + b)?,
-        Expression::Subtract(a, b) => eval_b(a, monkeys, memo)
-            .zip(eval_b(b, monkeys, memo))
-            .map(|(a, b)| a - b)?,
-        Expression::Multiply(a, b) => eval_b(a, monkeys, memo)
-            .zip(eval_b(b, monkeys, memo))
-            .map(|(a, b)| a * b)?,
-        Expression::Divide(a, b) => eval_b(a, monkeys, memo)
-            .zip(eval_b(b, monkeys, memo))
-            .map(|(a, b)| a / b)?,
+        Number(n) => n,
+        Operation((left_monkey, op, right_monkey)) => {
+            let left = fill_knowns(knowns, monkeys, left_monkey);
+            let right = fill_knowns(knowns, monkeys, right_monkey);
+            op.eval(left?, right?)
+        }
     };
-    memo.insert(monkey, val);
+    knowns.insert(monkey, val);
     Some(val)
 }
 
 fn part_b(data: &Parsed) -> i64 {
-    let Expression::Add(left, right) = data["root"] else {
-        panic!("root is not an add");
-    };
-    let mut memo = HashMap::new();
-    let (mut unknown_should_eq, mut unknown) = match (
-        eval_b(left, data, &mut memo),
-        eval_b(right, data, &mut memo),
-    ) {
-        (None, Some(val)) => (val, left),
-        (Some(val), None) => (val, right),
-        _ => panic!("exactly one side should be known"),
-    };
+    let mut knowns = HashMap::new();
+    fill_knowns(&mut knowns, data, "root");
+
+    let (mut unknown, mut unknown_should_eq) = ("root", 0);
     while unknown != "humn" {
-        (unknown_should_eq, unknown) = match data[unknown] {
-            Expression::Number(_) => panic!("unknown should not be a number"),
-            Expression::Add(left, right) => match (memo.get(&left), memo.get(&right)) {
-                (None, Some(val)) => (unknown_should_eq - val, left),
-                (Some(val), None) => (unknown_should_eq - val, right),
-                _ => panic!("exactly one side should be known"),
+        (unknown, unknown_should_eq) = match data[unknown] {
+            Operation((left, op, right)) => match (knowns.get(&left), knowns.get(&right)) {
+                (None, Some(&val)) if unknown == "root" => (left, val),
+                (Some(&val), None) if unknown == "root" => (right, val),
+                (None, Some(&val)) => (left, op.solve_for_left(unknown_should_eq, val)),
+                (Some(&val), None) => (right, op.solve_for_right(unknown_should_eq, val)),
+                _ => panic!("exactly one side of {unknown} should be known"),
             },
-            Expression::Subtract(left, right) => match (memo.get(&left), memo.get(&right)) {
-                (None, Some(val)) => (unknown_should_eq + val, left),
-                (Some(val), None) => (val - unknown_should_eq, right),
-                _ => panic!("exactly one side should be known"),
-            },
-            Expression::Multiply(left, right) => match (memo.get(&left), memo.get(&right)) {
-                (None, Some(val)) => (unknown_should_eq / val, left),
-                (Some(val), None) => (unknown_should_eq / val, right),
-                _ => panic!("exactly one side should be known"),
-            },
-            Expression::Divide(left, right) => match (memo.get(&left), memo.get(&right)) {
-                (None, Some(val)) => (unknown_should_eq * val, left),
-                (Some(val), None) => (val / unknown_should_eq, right),
-                _ => panic!("exactly one side should be known"),
-            },
+            Number(_) => panic!("{unknown} should not be a number"),
         }
     }
     unknown_should_eq
@@ -155,10 +163,8 @@ mod tests {
 }
 
 fn main() -> OutResult {
-    let start = std::time::Instant::now();
     let parsed = parse(DATA)?.1;
     println!("part a: {}", part_a(&parsed));
     println!("part b: {}", part_b(&parsed));
-    println!("time: {:?}", start.elapsed());
     Ok(())
 }
