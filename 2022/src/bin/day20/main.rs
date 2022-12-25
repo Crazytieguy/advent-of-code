e2b83a33ba100b2ce92cba8967d8df790c427cd5
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-25 03:35:02 +0200

    cool day 20 impl

--- 2022/src/bin/day20/main.rs
+++ 2022/src/bin/day20/main.rs
@@ -17,52 +17,77 @@ fn parse(data: &str) -> IResult<Parsed> {
     separated_list1(line_ending, i64)(data)
 }
 
-fn solve(data: &Parsed, decryption_key: i64, iterations: usize) -> i64 {
+fn solve<const NEXT_SIZE: usize>(data: &Parsed, decryption_key: i64, iterations: usize) -> i64 {
     let numbers = data.iter().map(|&x| x * decryption_key).collect_vec();
     let mut prev = (0..numbers.len() as u16).collect_vec();
     let mut next = prev.clone();
     prev.rotate_right(1);
-    next.rotate_left(1);
+    next.rotate_left(NEXT_SIZE % numbers.len());
     for _ in 0..iterations {
         for (cur, &n) in numbers.iter().enumerate() {
-            let amount_to_move = n.rem_euclid(numbers.len() as i64 - 1) as usize;
-
             // remove cur from the list
-            let (cur_prev, cur_next) = (prev[cur], next[cur]);
-            prev[cur_next as usize] = cur_prev;
-            next[cur_prev as usize] = cur_next;
+            fix_pairs_backwards(prev[cur], next[cur], &mut prev, &mut next, cur as u16);
 
             // find the node to insert cur after
-            let target = iterate(cur_prev, |&cur| next[cur as usize])
-                .nth(amount_to_move)
-                .unwrap();
+            let amount_to_move = n.rem_euclid(numbers.len() as i64 - 1) as usize;
+            let target = find_target::<NEXT_SIZE>(prev[cur], amount_to_move, &prev, &next);
 
             // insert cur after target
-            let target_next = next[target as usize];
             prev[cur] = target;
-            next[target as usize] = cur as u16;
-            prev[target_next as usize] = cur as u16;
-            next[cur] = target_next;
+            fix_pairs_backwards(
+                cur as u16,
+                next[target as usize],
+                &mut prev,
+                &mut next,
+                target,
+            );
         }
     }
     let zero_index = numbers
         .iter()
         .position(|&x| x == 0)
-        .expect("an element with value 0") as u16;
-    iterate(zero_index, |&cur| next[cur as usize])
-        .step_by(1000)
-        .skip(1)
-        .take(3)
-        .map(|i| numbers[i as usize])
-        .sum()
+        .expect("an element with value 0");
+    iterate(zero_index as u16, |&cur| {
+        find_target::<NEXT_SIZE>(cur, 1000, &prev, &next)
+    })
+    .skip(1)
+    .take(3)
+    .map(|i| numbers[i as usize])
+    .sum()
 }
 
-fn part_a(data: &Parsed) -> i64 {
-    solve(data, 1, 1)
+fn fix_pairs_backwards(left: u16, right: u16, prev: &mut [u16], next: &mut [u16], stop: u16) {
+    let (far_prev, immediate_next) = iterate(left, |&i| prev[i as usize])
+        .zip(iterate(right, |&i| prev[i as usize]))
+        .inspect(|&(before, after)| {
+            next[before as usize] = after;
+        })
+        .find(|&(_, after)| prev[after as usize] == stop)
+        .unwrap();
+    prev[immediate_next as usize] = left;
+    next[prev[far_prev as usize] as usize] = left;
 }
 
-fn part_b(data: &Parsed) -> i64 {
-    solve(data, 811589153, 10)
+fn find_target<const NEXT_SIZE: usize>(
+    from: u16,
+    amount_to_move: usize,
+    prev: &[u16],
+    next: &[u16],
+) -> u16 {
+    let overshot_target = iterate(from, |&cur| next[cur as usize])
+        .nth((NEXT_SIZE + amount_to_move) / NEXT_SIZE)
+        .unwrap();
+    iterate(overshot_target, |&cur| prev[cur as usize])
+        .nth(NEXT_SIZE - amount_to_move % NEXT_SIZE)
+        .unwrap()
+}
+
+fn part_a<const NEXT_SIZE: usize>(data: &Parsed) -> i64 {
+    solve::<NEXT_SIZE>(data, 1, 1)
+}
+
+fn part_b<const NEXT_SIZE: usize>(data: &Parsed) -> i64 {
+    solve::<NEXT_SIZE>(data, 811589153, 10)
 }
 
 #[cfg(test)]
@@ -72,22 +97,22 @@ mod tests {
 
     #[test]
     fn test_a() -> OutResult {
-        assert_eq!(part_a(&parse(SAMPLE_DATA)?.1), 3);
-        println!("part a: {}", part_a(&parse(DATA)?.1));
+        assert_eq!(part_a::<1>(&parse(SAMPLE_DATA)?.1), 3);
+        println!("part a: {}", part_a::<25>(&parse(DATA)?.1));
         Ok(())
     }
 
     #[test]
     fn test_b() -> OutResult {
-        assert_eq!(part_b(&parse(SAMPLE_DATA)?.1), 1623178306);
-        println!("part b: {}", part_b(&parse(DATA)?.1));
+        assert_eq!(part_b::<1>(&parse(SAMPLE_DATA)?.1), 1623178306);
+        println!("part b: {}", part_b::<25>(&parse(DATA)?.1));
         Ok(())
     }
 }
 
 fn main() -> OutResult {
     let parsed = parse(DATA)?.1;
-    println!("part a: {}", part_a(&parsed));
-    println!("part b: {}", part_b(&parsed));
+    println!("part a: {}", part_a::<25>(&parsed));
+    println!("part b: {}", part_b::<25>(&parsed));
     Ok(())
 }
