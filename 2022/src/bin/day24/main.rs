fed4eb3be6e2db523c4a5ca1289e1bd6239964dd
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-01-06 14:38:53 +0200

    remove bitvec dep

--- 2022/src/bin/day24/main.rs
+++ 2022/src/bin/day24/main.rs
@@ -1,74 +1,86 @@
-use std::error::Error;
+use std::{collections::VecDeque, error::Error};
 
-use bitvec::prelude::BitArray;
-use itertools::Itertools;
+use itertools::izip;
 
 const DATA: &str = include_str!("data.txt");
 
 type OutResult = std::result::Result<(), Box<dyn Error>>;
 
-type BitGrid = [BitArray<[u64; 2]>; 25];
-
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone)]
 struct Blizzards {
-    up: BitGrid,
-    down: BitGrid,
-    left: BitGrid,
-    right: BitGrid,
+    up: VecDeque<u128>,
+    down: VecDeque<u128>,
+    left: Vec<u128>,
+    right: Vec<u128>,
+}
+
+fn mask(width: usize) -> u128 {
+    (1 << width) - 1
+}
+
+fn rotate_start(row: &mut u128, width: usize) {
+    *row = (*row >> 1) | ((*row & 1) << (width - 1));
+    *row &= mask(width);
+}
+
+fn rotate_end(row: &mut u128, width: usize) {
+    *row = (*row << 1) | (*row >> (width - 1));
+    *row &= mask(width);
 }
 
 impl Blizzards {
-    fn update(&mut self, height: usize, width: usize) {
-        self.up[..height].rotate_left(1);
-        self.down[..height].rotate_right(1);
+    fn update(&mut self, width: usize) {
+        self.up.rotate_left(1);
+        self.down.rotate_right(1);
         self.left
             .iter_mut()
-            .for_each(|row| row[..width].rotate_left(1));
-        self.right
-            .iter_mut()
-            .for_each(|row| row[..width].rotate_right(1));
+            .for_each(|row| rotate_start(row, width));
+        self.right.iter_mut().for_each(|row| rotate_end(row, width));
     }
 }
 
-fn parse(data: &str) -> (Blizzards, usize, usize) {
-    let lines = data.lines().collect_vec();
-    let height = lines.len() - 2;
-    let width = lines[0].len() - 2;
-    let mut blizzards = Blizzards::default();
-    lines
-        .into_iter()
+fn parse(data: &str) -> (Blizzards, usize) {
+    let width = data.find('\n').expect("no newline") - 2;
+    let (up, (down, (left, right))) = data
+        .lines()
         .filter(|line| &line[2..3] != "#")
-        .enumerate()
-        .for_each(|(row, line)| {
-            line.chars()
-                .filter(|&c| c != '#')
+        .map(|line| {
+            let (mut up, mut down, mut left, mut right) = (0, 0, 0, 0);
+            line.bytes()
+                .filter(|&c| c != b'#')
                 .enumerate()
                 .for_each(|(col, c)| {
+                    let bit = 1 << col;
                     match c {
-                        '>' => blizzards.right[row].set(col, true),
-                        '<' => blizzards.left[row].set(col, true),
-                        '^' => blizzards.up[row].set(col, true),
-                        'v' => blizzards.down[row].set(col, true),
+                        b'>' => right |= bit,
+                        b'<' => left |= bit,
+                        b'^' => up |= bit,
+                        b'v' => down |= bit,
                         _ => {}
                     };
-                })
-        });
-    (blizzards, height, width)
+                });
+            (up, (down, (left, right)))
+        })
+        .unzip();
+    (
+        Blizzards {
+            up,
+            down,
+            left,
+            right,
+        },
+        width,
+    )
 }
 
-fn adjacent_positions(positions: &BitGrid, height: usize, width: usize) -> BitGrid {
-    let mut next_positions = *positions;
-    for row in 0..height {
-        next_positions[row][1..width] |= &positions[row][0..width - 1];
-        next_positions[row][0..width - 1] |= &positions[row][1..width];
-        if row > 0 {
-            next_positions[row] |= &positions[row - 1];
-        }
-        if row < height - 1 {
-            next_positions[row] |= &positions[row + 1];
-        }
-    }
-    next_positions
+fn adjacent_positions(positions: &[u128], width: usize) -> Vec<u128> {
+    izip!(
+        [0].iter().chain(positions),
+        positions,
+        positions[1..].iter().chain([0].iter())
+    )
+    .map(|(&above, &cur, &below)| (cur | cur << 1 | cur >> 1 | above | below) & mask(width))
+    .collect()
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -79,45 +91,46 @@ enum Destination {
 
 use Destination::*;
 
-#[allow(clippy::needless_range_loop)]
 fn simulate_shortest_path(
     blizzards: &mut Blizzards,
-    height: usize,
     width: usize,
     destination: Destination,
 ) -> usize {
-    let mut positions = BitGrid::default();
+    let mut positions = vec![0; blizzards.right.len()];
     for minute in 1.. {
-        blizzards.update(height, width);
-        positions = adjacent_positions(&positions, height, width);
+        blizzards.update(width);
+        positions = adjacent_positions(&positions, width);
         match destination {
-            Exit => positions[0].set(0, true),
-            Entrance => positions[height - 1].set(width - 1, true),
+            Exit => positions[0] |= 1,
+            Entrance => *positions.last_mut().unwrap() |= 1 << (width - 1),
         }
-        for row in 0..height {
-            positions[row] &= &!(blizzards.up[row]
-                | blizzards.down[row]
-                | blizzards.left[row]
-                | blizzards.right[row]);
+        for (p, up, down, left, right) in izip!(
+            &mut positions,
+            &blizzards.up,
+            &blizzards.down,
+            &blizzards.left,
+            &blizzards.right
+        ) {
+            *p &= !(up | down | left | right);
         }
-        if matches!(destination, Exit) && positions[height - 1][width - 1]
-            || matches!(destination, Entrance) && positions[0][0]
+        if matches!(destination, Exit) && positions.last().unwrap() >> (width - 1) == 1
+            || matches!(destination, Entrance) && positions[0] & 1 == 1
         {
-            blizzards.update(height, width);
+            blizzards.update(width);
             return minute + 1;
         }
     }
     unreachable!()
 }
 
-fn part_a((mut blizzards, height, width): (Blizzards, usize, usize)) -> usize {
-    simulate_shortest_path(&mut blizzards, height, width, Exit)
+fn part_a(blizzards: &mut Blizzards, width: usize) -> usize {
+    simulate_shortest_path(blizzards, width, Exit)
 }
 
-fn part_b((mut blizzards, height, width): (Blizzards, usize, usize)) -> usize {
-    simulate_shortest_path(&mut blizzards, height, width, Exit)
-        + simulate_shortest_path(&mut blizzards, height, width, Entrance)
-        + simulate_shortest_path(&mut blizzards, height, width, Exit)
+fn part_b(blizzards: &mut Blizzards, width: usize, part_a_ans: usize) -> usize {
+    part_a_ans
+        + simulate_shortest_path(blizzards, width, Entrance)
+        + simulate_shortest_path(blizzards, width, Exit)
 }
 
 #[cfg(test)]
@@ -127,22 +140,27 @@ mod tests {
 
     #[test]
     fn test_a() -> OutResult {
-        assert_eq!(part_a(parse(SAMPLE_DATA)), 18);
-        println!("part a: {}", part_a(parse(DATA)));
+        let (mut blizzards, width) = parse(SAMPLE_DATA);
+        assert_eq!(part_a(&mut blizzards, width), 18);
         Ok(())
     }
 
     #[test]
     fn test_b() -> OutResult {
-        assert_eq!(part_b(parse(SAMPLE_DATA)), 54);
-        println!("part b: {}", part_b(parse(DATA)));
+        let (mut blizzards, width) = parse(SAMPLE_DATA);
+        let a = part_a(&mut blizzards, width);
+        assert_eq!(part_b(&mut blizzards, width, a), 54);
         Ok(())
     }
 }
 
 fn main() -> OutResult {
-    let parsed = parse(DATA);
-    println!("part a: {}", part_a(parsed));
-    println!("part b: {}", part_b(parsed));
+    let (mut blizzards, width) = parse(DATA);
+    let start = std::time::Instant::now();
+    let a = part_a(&mut blizzards, width);
+    println!("part a: {a}");
+    let b = part_b(&mut blizzards, width, a);
+    println!("part b: {b}");
+    println!("{:?}", start.elapsed());
     Ok(())
 }
