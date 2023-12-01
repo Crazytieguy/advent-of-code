a53c010afc59c4c75da9a8213b1be94309cdcfd5
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-12-01 19:20:53 +0100

    refactor day 1

--- 2023/src/bin/day1/main.rs
+++ 2023/src/bin/day1/main.rs
@@ -1,6 +1,5 @@
 use advent_2023::*;
 use anyhow::anyhow;
-use itertools::Itertools;
 
 struct Day;
 
@@ -20,72 +19,46 @@ impl BasicSolution for Day {
     }
 
     fn a(data: Self::Parsed) -> anyhow::Result<Self::Answer> {
-        solve(data, DIGIT_LITERAL_VALUES)
+        solve(data, &[])
     }
 
     fn b(data: Self::Parsed) -> anyhow::Result<Self::Answer> {
         solve(
             data,
-            DIGIT_LITERAL_VALUES
-                .into_iter()
-                .chain(SPELLED_OUT_DIGIT_VALUES),
+            &[
+                ("one", 1),
+                ("two", 2),
+                ("three", 3),
+                ("four", 4),
+                ("five", 5),
+                ("six", 6),
+                ("seven", 7),
+                ("eight", 8),
+                ("nine", 9),
+            ],
         )
     }
 }
 
-const DIGIT_LITERAL_VALUES: [(&str, u32); 9] = [
-    ("1", 1),
-    ("2", 2),
-    ("3", 3),
-    ("4", 4),
-    ("5", 5),
-    ("6", 6),
-    ("7", 7),
-    ("8", 8),
-    ("9", 9),
-];
-
-const SPELLED_OUT_DIGIT_VALUES: [(&str, u32); 9] = [
-    ("one", 1),
-    ("two", 2),
-    ("three", 3),
-    ("four", 4),
-    ("five", 5),
-    ("six", 6),
-    ("seven", 7),
-    ("eight", 8),
-    ("nine", 9),
-];
-
-fn solve<'a, P>(data: &'a str, patterns: P) -> anyhow::Result<u32>
-where
-    P: IntoIterator<Item = (&'a str, u32)> + Clone,
-    P::IntoIter: Clone,
-{
+fn solve(data: &str, spelled_out_vals: &[(&str, u32)]) -> anyhow::Result<u32> {
     itertools::process_results(
-        data.lines().map(|line| calibration_value(line, &patterns)),
+        data.lines()
+            .map(|line| calibration_value(line, spelled_out_vals)),
         |it| it.sum(),
     )
 }
 
-fn calibration_value<'a, P>(line: &'a str, patterns: &P) -> anyhow::Result<u32>
-where
-    P: IntoIterator<Item = (&'a str, u32)> + Clone,
-    P::IntoIter: Clone,
-{
+fn calibration_value(line: &str, spelled_out_vals: &[(&str, u32)]) -> anyhow::Result<u32> {
     let err = || anyhow!("Couldn't find a digit in line '{line}'");
-    let value_if_pat_at_i = |(i, (pat, value))| line[i..].starts_with(pat).then_some(value);
 
-    let first = (0..line.len())
-        .cartesian_product(patterns.clone())
-        .find_map(value_if_pat_at_i)
-        .ok_or_else(err)?;
+    let digit_at_i = |i| {
+        let literal = line[i..i + 1].parse().ok();
+        let match_spelled = |&(digit, val)| line[i..].starts_with(digit).then_some(val);
+        literal.or_else(|| spelled_out_vals.iter().find_map(match_spelled))
+    };
 
-    let last = (0..line.len())
-        .rev()
-        .cartesian_product(patterns.clone())
-        .find_map(value_if_pat_at_i)
-        .ok_or_else(err)?;
+    let first = (0..line.len()).find_map(digit_at_i).ok_or_else(err)?;
+    let last = (0..line.len()).rev().find_map(digit_at_i).ok_or_else(err)?;
 
     Ok(first * 10 + last)
 }
