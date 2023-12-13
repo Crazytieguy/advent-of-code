b8c52f49a7fb5f8d81f1021d28a605f749bebbc2
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-12-13 08:27:47 +0100

    rust performance optimization

--- 2023/src/bin/day13/main.rs
+++ 2023/src/bin/day13/main.rs
@@ -9,7 +9,7 @@ impl BasicSolution for Day {
     const INPUT: &'static str = include_str!("data.txt");
     const SAMPLE_INPUT: &'static str = include_str!("sample.txt");
 
-    type Shared = Vec<Vec<&'static str>>;
+    type Shared = Vec<Vec<&'static [u8]>>;
     type Answer = usize;
 
     const SAMPLE_ANSWER_A: Self::TestAnswer = 405;
@@ -18,7 +18,7 @@ impl BasicSolution for Day {
     fn shared(input: &'static str) -> anyhow::Result<Self::Shared> {
         Ok(input
             .split("\n\n")
-            .map(|note| note.lines().collect())
+            .map(|note| note.lines().map(str::as_bytes).collect())
             .collect())
     }
 
@@ -31,35 +31,32 @@ impl BasicSolution for Day {
     }
 }
 
-fn score(note: &[&str], allowed_mismatches: usize) -> anyhow::Result<usize> {
-    if let Some(mirror_column) =
-        find_reflection_index(note.iter().map(|row| row.bytes()), allowed_mismatches)
-    {
-        Ok(mirror_column)
-    } else if let Some(mirror_row) = find_reflection_index(
-        (0..note[0].len()).map(|i| note.iter().map(move |row| row.as_bytes()[i])),
+fn score(note: &[&[u8]], allowed_mismatches: usize) -> anyhow::Result<usize> {
+    if let Some(mirror_row) = find_reflection_index(
+        note.iter().map(|row| row.iter().copied()),
         allowed_mismatches,
     ) {
         Ok(mirror_row * 100)
+    } else if let Some(mirror_column) = find_reflection_index(
+        (0..note[0].len()).map(|i| note.iter().map(move |row| row[i])),
+        allowed_mismatches,
+    ) {
+        Ok(mirror_column)
     } else {
         Err(anyhow!("No mirror found for note\n{note:?}"))
     }
 }
 
-fn find_reflection_index<T: DoubleEndedIterator<Item = u8> + ExactSizeIterator + Clone>(
-    note: impl Iterator<Item = T> + Clone,
+fn find_reflection_index<T: Iterator<Item = u8> + Clone>(
+    note: impl DoubleEndedIterator<Item = T> + ExactSizeIterator + Clone,
     allowed_mismatches: usize,
 ) -> Option<usize> {
-    (1..note.clone().next()?.len()).find(|&i| {
+    (1..note.len()).find(|&i| {
         note.clone()
-            .map(|row| {
-                row.clone()
-                    .take(i)
-                    .rev()
-                    .zip(row.skip(i))
-                    .filter(|(a, b)| a != b)
-                    .count()
-            })
+            .take(i)
+            .rev()
+            .zip(note.clone().skip(i))
+            .map(|(a, b)| a.zip(b).filter(|(a, b)| a != b).count())
             .sum::<usize>()
             == allowed_mismatches
     })
