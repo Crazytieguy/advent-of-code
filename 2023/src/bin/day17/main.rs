9d932044ec9989b5fd26f684301e3e99df198d80
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-12-21 13:53:37 +0100

    2023 day 17 rust refactor

--- 2023/src/bin/day17/main.rs
+++ 2023/src/bin/day17/main.rs
@@ -9,19 +9,12 @@ use anyhow::anyhow;
 
 struct Day;
 
-#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct StateA {
-    row: usize,
-    col: usize,
-    last_three_moves: [(isize, isize); 3],
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct StateB {
-    row: usize,
-    col: usize,
-    current_direction: (isize, isize),
-    has_moved: usize,
+struct State {
+    row: u8,
+    col: u8,
+    current_direction: (i8, i8),
+    has_moved: u8,
 }
 
 impl BasicSolution for Day {
@@ -35,7 +28,7 @@ impl BasicSolution for Day {
 ";
 
     type Shared = Vec<&'static [u8]>;
-    type Answer = u32;
+    type Answer = u16;
 
     const SAMPLE_ANSWER_A: Self::TestAnswer = 102;
     const SAMPLE_ANSWER_B: Self::TestAnswer = 71;
@@ -45,106 +38,92 @@ impl BasicSolution for Day {
     }
 
     fn part_a(map: Cow<Self::Shared>) -> anyhow::Result<Self::Answer> {
-        let initial_state = StateA::default();
-        let mut queue = BinaryHeap::from([(Reverse(0), initial_state)]);
-        let mut seen = HashSet::new();
-        while let Some((Reverse(total_heat_loss), state)) = queue.pop() {
-            if (state.row, state.col) == (map.len() - 1, map[0].len() - 1) {
-                return Ok(total_heat_loss);
-            }
-            if !seen.insert(state.clone()) {
-                continue;
-            }
-            for (drow, dcol) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                if state.last_three_moves[2] == (-drow, -dcol) {
-                    continue;
-                }
-                if state.last_three_moves == [(drow, dcol); 3] {
-                    continue;
-                }
-                let Some(row) = state.row.checked_add_signed(drow) else {
-                    continue;
-                };
-                let Some(col) = state.col.checked_add_signed(dcol) else {
-                    continue;
-                };
-                let Some(heat_loss) = map.get(row).and_then(|row| row.get(col)) else {
-                    continue;
-                };
-                let heat_loss = heat_loss - b'0';
-                queue.push((
-                    Reverse(total_heat_loss + heat_loss as u32),
-                    StateA {
-                        row,
-                        col,
-                        last_three_moves: [
-                            state.last_three_moves[1],
-                            state.last_three_moves[2],
-                            (drow, dcol),
-                        ],
-                    },
-                ))
-            }
-        }
-        Err(anyhow!("No path found"))
+        let initial_state = State {
+            row: 0,
+            col: 0,
+            current_direction: (0, 0),
+            has_moved: 0,
+        };
+        djkstra(
+            &map,
+            initial_state,
+            |state, (drow, dcol)| state.has_moved == 3 && state.current_direction == (drow, dcol),
+            |state| (state.row as usize, state.col as usize) == (map.len() - 1, map[0].len() - 1),
+        )
     }
 
     fn part_b(map: Self::Shared) -> anyhow::Result<Self::Answer> {
-        let initial_state = StateB {
+        let initial_state = State {
             row: 0,
             col: 0,
             current_direction: (0, 0),
             has_moved: 4,
         };
-        let mut queue = BinaryHeap::from([(Reverse(0), initial_state)]);
-        let mut seen = HashSet::new();
-        while let Some((Reverse(total_heat_loss), state)) = queue.pop() {
-            if (state.row, state.col) == (map.len() - 1, map[0].len() - 1) {
-                if state.has_moved < 4 {
-                    continue;
-                }
-                return Ok(total_heat_loss);
-            }
-            if !seen.insert(state.clone()) {
+        djkstra(
+            &map,
+            initial_state,
+            |state, (drow, dcol)| {
+                (state.has_moved < 4 && state.current_direction != (drow, dcol))
+                    || (state.has_moved == 10 && state.current_direction == (drow, dcol))
+            },
+            |state| {
+                state.has_moved >= 4
+                    && (state.row as usize, state.col as usize) == (map.len() - 1, map[0].len() - 1)
+            },
+        )
+    }
+}
+
+fn djkstra(
+    map: &[&[u8]],
+    initial_state: State,
+    invalid_move: impl Fn(&State, (i8, i8)) -> bool,
+    target: impl Fn(&State) -> bool,
+) -> Result<u16, anyhow::Error> {
+    let mut queue = BinaryHeap::from([(Reverse(0), initial_state)]);
+    let mut seen = HashSet::new();
+    while let Some((Reverse(total_heat_loss), state)) = queue.pop() {
+        if target(&state) {
+            return Ok(total_heat_loss);
+        }
+        if !seen.insert(state.clone()) {
+            continue;
+        }
+        for (drow, dcol) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
+            if state.current_direction == (-drow, -dcol) {
                 continue;
             }
-            for (drow, dcol) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                if state.current_direction == (-drow, -dcol) {
-                    continue;
-                }
-                if state.has_moved < 4 && state.current_direction != (drow, dcol) {
-                    continue;
-                }
-                if state.has_moved == 10 && state.current_direction == (drow, dcol) {
-                    continue;
-                }
-                let Some(row) = state.row.checked_add_signed(drow) else {
-                    continue;
-                };
-                let Some(col) = state.col.checked_add_signed(dcol) else {
-                    continue;
-                };
-                let Some(heat_loss) = map.get(row).and_then(|row| row.get(col)) else {
-                    continue;
-                };
-                let heat_loss = heat_loss - b'0';
-                queue.push((
-                    Reverse(total_heat_loss + heat_loss as u32),
-                    StateB {
-                        row,
-                        col,
-                        current_direction: (drow, dcol),
-                        has_moved: if state.current_direction == (drow, dcol) {
-                            state.has_moved + 1
-                        } else {
-                            1
-                        },
-                    },
-                ))
+            if invalid_move(&state, (drow, dcol)) {
+                continue;
             }
+            let Some(row) = state.row.checked_add_signed(drow) else {
+                continue;
+            };
+            let Some(col) = state.col.checked_add_signed(dcol) else {
+                continue;
+            };
+            let Some(heat_loss) = map.get(row as usize).and_then(|row| row.get(col as usize))
+            else {
+                continue;
+            };
+            let heat_loss = heat_loss - b'0';
+            let has_moved = if state.current_direction == (drow, dcol) {
+                state.has_moved + 1
+            } else {
+                1
+            };
+            queue.push((
+                Reverse(total_heat_loss + heat_loss as u16),
+                State {
+                    row,
+                    col,
+                    current_direction: (drow, dcol),
+                    has_moved,
+                },
+            ))
         }
-        Err(anyhow!("No path found"))
     }
+    Err(anyhow!("No path found"))
 }
 
 fn main() -> anyhow::Result<()> {
