9a89f55a79d4d196361040cbd4e4ac159a8e9ea1
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-12-22 11:03:27 +0100

    2023 day 22 rust refactor

--- 2023/src/bin/day22/main.rs
+++ 2023/src/bin/day22/main.rs
@@ -40,7 +40,7 @@ impl BasicSolution for Day {
         let (bricks, grid, ends) = shared.as_ref();
         let mut total = 0;
         for brick_id in 1..=bricks.len() as u16 {
-            if find_exclusively_supported(bricks, grid, ends, brick_id).is_empty() {
+            if find_supported::<true>(bricks, grid, ends, brick_id).is_empty() {
                 total += 1;
             }
         }
@@ -49,64 +49,37 @@ impl BasicSolution for Day {
 
     fn part_b((bricks, grid, end): Self::Shared) -> anyhow::Result<Self::Answer> {
         let supported = (1..=bricks.len() as u16)
-            .map(|brick_id| find_supported(&bricks, &grid, &end, brick_id))
+            .map(|brick_id| find_supported::<false>(&bricks, &grid, &end, brick_id))
             .collect_vec();
-        let mut initial_supported_counts = vec![0; supported.len()];
+        let mut initial_supported_counts = vec![0; bricks.len()];
         for supported_id in supported.iter().flatten() {
             initial_supported_counts[*supported_id as usize - 1] += 1;
         }
-        for x in 0..10 {
-            for y in 0..10 {
-                if grid[1][x][y] != 0 {
-                    initial_supported_counts[grid[1][x][y] as usize - 1] += 1;
-                }
+        for (x, y) in (0..10).cartesian_product(0..10) {
+            if grid[1][x][y] != 0 {
+                initial_supported_counts[grid[1][x][y] as usize - 1] += 1;
             }
         }
         Ok((1..=bricks.len() as u16)
-            .map(|remove| chain_reaction(&supported, initial_supported_counts.clone(), remove))
+            .map(|remove| {
+                let mut supported_counts = initial_supported_counts.clone();
+                chain_reaction(&supported, &mut supported_counts, remove);
+                supported_counts.iter().filter(|&&count| count == 0).count()
+            })
             .sum())
     }
 }
 
-fn chain_reaction(
-    supported: &[FxHashSet<u16>],
-    mut supported_counts: Vec<usize>,
-    remove: u16,
-) -> usize {
-    let mut queue = vec![remove];
-    let mut seen = FxHashSet::default();
-    while let Some(brick_id) = queue.pop() {
-        if !seen.insert(brick_id) {
-            continue;
-        }
-        for supported_id in &supported[brick_id as usize - 1] {
-            supported_counts[*supported_id as usize - 1] -= 1;
-            if supported_counts[*supported_id as usize - 1] == 0 {
-                queue.push(*supported_id);
-            }
+fn chain_reaction(supported: &[FxHashSet<u16>], supported_counts: &mut [usize], remove: u16) {
+    for &supported_id in &supported[remove as usize - 1] {
+        supported_counts[supported_id as usize - 1] -= 1;
+        if supported_counts[supported_id as usize - 1] == 0 {
+            chain_reaction(supported, supported_counts, supported_id);
         }
     }
-    supported_counts.iter().filter(|&&count| count == 0).count()
 }
 
-fn find_supported(bricks: &[Brick], grid: &Grid, ends: &Ends, brick_id: u16) -> FxHashSet<u16> {
-    let mut supported_ids = FxHashSet::default();
-    let brick = &bricks[brick_id as usize - 1];
-    let end = ends[brick_id as usize - 1];
-    if let Some(floor_above) = grid.get(end as usize + 1) {
-        for x in brick.from.0..=brick.to.0 {
-            for y in brick.from.1..=brick.to.1 {
-                let supported_id = floor_above[x as usize][y as usize];
-                if supported_id != 0 {
-                    supported_ids.insert(supported_id);
-                }
-            }
-        }
-    }
-    supported_ids
-}
-
-fn find_exclusively_supported(
+fn find_supported<const EXCLUSIVE: bool>(
     bricks: &[Brick],
     grid: &Grid,
     ends: &Ends,
@@ -116,20 +89,18 @@ fn find_exclusively_supported(
     let brick = &bricks[brick_id as usize - 1];
     let end = ends[brick_id as usize - 1];
     if let Some(floor_above) = grid.get(end as usize + 1) {
-        for x in brick.from.0..=brick.to.0 {
-            for y in brick.from.1..=brick.to.1 {
-                let supported_id = floor_above[x as usize][y as usize];
-                if supported_id != 0 {
+        for (x, y) in brick.xys() {
+            let supported_id = floor_above[x][y];
+            if supported_id != 0 {
+                let should_insert = !EXCLUSIVE || {
                     let supported_brick = &bricks[supported_id as usize - 1];
-                    if (supported_brick.from.0..=supported_brick.to.0)
-                        .cartesian_product(supported_brick.from.1..=supported_brick.to.1)
-                        .all(|(x, y)| {
-                            let supported_by = &grid[end as usize][x as usize][y as usize];
-                            [0, brick_id].contains(supported_by)
-                        })
-                    {
-                        supported_ids.insert(supported_id);
-                    }
+                    supported_brick.xys().all(|(x, y)| {
+                        let supported_by = &grid[end as usize][x][y];
+                        [0, brick_id].contains(supported_by)
+                    })
+                };
+                if should_insert {
+                    supported_ids.insert(supported_id);
                 }
             }
         }
@@ -145,32 +116,32 @@ fn compute_resting_position(bricks: &mut [Brick]) -> anyhow::Result<(Grid, Vec<u
         let start = (1..=brick.from.2)
             .rev()
             .take_while(|&start| {
-                let collision = grid.get(start as usize).is_some_and(|floor| {
-                    for x in brick.from.0..=brick.to.0 {
-                        for y in brick.from.1..=brick.to.1 {
-                            if floor[x as usize][y as usize] != 0 {
-                                return true;
-                            }
-                        }
-                    }
-                    false
-                });
+                let collision = grid
+                    .get(start as usize)
+                    .is_some_and(|floor| brick.xys().any(|(x, y)| floor[x][y] != 0));
                 !collision
             })
             .last()
             .ok_or_else(|| anyhow!("No start found for brick {brick:?}"))?;
         let end = start + brick.to.2 - brick.from.2;
         ends.push(end);
-        grid.resize_with(grid.len().max(end as usize + 1), Default::default);
+        while grid.len() <= end as usize {
+            grid.push(Default::default());
+        }
         for z in start..=end {
-            for x in brick.from.0..=brick.to.0 {
-                for y in brick.from.1..=brick.to.1 {
-                    grid[z as usize][x as usize][y as usize] = brick_id;
-                }
+            for (x, y) in brick.xys() {
+                grid[z as usize][x][y] = brick_id;
             }
         }
     }
     Ok((grid, ends))
+}
+
+impl Brick {
+    fn xys(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (self.from.0 as usize..=self.to.0 as usize)
+            .cartesian_product(self.from.1 as usize..=self.to.1 as usize)
+    }
 }
 
 fn brick(input: &mut &'static str) -> winnow::PResult<Brick> {
