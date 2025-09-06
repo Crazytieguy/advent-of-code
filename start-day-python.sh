d1f2cb49f852edd4c454e61413c2982c450fde7e
Yoav Tzfati <yoav.tzfati@gmail.com>
2025-09-05 22:24:49 -0700

    uv setup

--- start-day-python.sh
+++ start-day-python.sh
@@ -3,4 +3,4 @@
 mkdir -p $1/python/day$2
 cp -n $1/python/template/* $1/python/day$2/
 curl https://adventofcode.com/$1/day/$2/input --cookie "session=$(cat session)" > $1/python/day$2/data.txt
-cd $1/python/day$2 && ptw -q -- main.py
+cd $1/python/day$2 && uv run --dev ptw . --now -- main.py
